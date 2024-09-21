30f2811c42901354f03c1476ccae3e381a5c6e89
Piyush-Das <piyushdas3110@gmail.com>
2024-09-22 03:07:48 +0530

    created a navbarLinks components

--- shophekart-frontend/src/app/page.tsx
+++ shophekart-frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '@/components/Hero/Navbar'
+import Navbar from '@/components/Hero/Navbar/Navbar'
 const page = () => {
   return (
     <div className='bg-[#f1f4ff]'>
---
+++ shophekart-frontend/src/components/Hero/Navbar/NavbarLinks.tsx
@@ -0,0 +1,15 @@
+import NavLink from './NavLink';
+
+const NavbarLinks = () => {
+  return (
+    <div className="flex space-x-8">
+      <NavLink href="#">Buy $CSHOP</NavLink>
+      <NavLink href="#">Staking</NavLink>
+      <NavLink href="#">Product</NavLink>
+      <NavLink href="#">CSHOP Card</NavLink>
+      <NavLink href="#">Whitepaper</NavLink>
+    </div>
+  );
+};
+
+export default NavbarLinks;

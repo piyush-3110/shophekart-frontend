ff6259b9b489412d698408c652dc1c58048d8d85
Piyush-Das <piyushdas3110@gmail.com>
2024-09-22 03:09:12 +0530

    created a connectwallet component

---
+++ shophekart-frontend/src/components/Hero/Navbar/ConnectWallet.tsx
@@ -0,0 +1,9 @@
+const ConnectButton = () => {
+    return (
+      <button className="border-[#2546fe] text-[#2546fe] border-2 py-2 px-4 rounded hover:text-[#253384]">
+        Connect Wallet
+      </button>
+    );
+  };
+  
+  export default ConnectButton;

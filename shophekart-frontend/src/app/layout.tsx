0521262c0e0379e738c4a5f2da1e938426e0fa56
Piyush-Das <piyushdas3110@gmail.com>
2024-09-22 04:54:29 +0530

    added font

--- shophekart-frontend/src/app/layout.tsx
+++ shophekart-frontend/src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {DM_Sans} from "next/font/google"
 import "./globals.css";
+ const dm = DM_Sans({
+  subsets:['latin'],
+  weight:['400','500','700'],
+ 
+ })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "ShophEkart",
@@ -30,7 +25,7 @@ export default function RootLayout({
       <link rel="icon" href="./images/shared/favicon.png" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dm.className} antialiased`}
       >
         {children}
       </body>
--- shophekart-frontend/src/app/page.tsx
+++ shophekart-frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Navbar from '@/components/Hero/Navbar/Navbar'
+
 import FloatingNavbar from '@/components/Hero/Navbar/FloatingNavbar'
 const page = () => {
   return (
-    <div className='bg-[#f1f4ff] h-[200vh] '>
+    <div className='bg-[#f1f4ff]  h-[200vh] '>
       <FloatingNavbar/>
     </div>
   )

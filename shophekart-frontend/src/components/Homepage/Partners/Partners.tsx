583f0a5faabf1dfee0fde90ff1fbbdf4d9651309
Piyush-Das <piyushdas3110@gmail.com>
2024-09-22 20:13:13 +0530

    added image of mastercard

--- shophekart-frontend/src/components/Homepage/Partners/Partners.tsx
+++ shophekart-frontend/src/components/Homepage/Partners/Partners.tsx
@@ -1,34 +1,53 @@
-import React from 'react'
-
-import MarqueeComp from './MarqueeComp'
+import React from 'react';
+import Image from 'next/image';
+import MarqueeComp from './MarqueeComp';
 
 const Partners = () => {
   return (
     <div className='min-h-[100vh] py-12 flex gap-8 flex-col items-center bg-white'>
-        <h1 className='text-black font-semibold md:text-3xl text-lg text-center'>Partners and Supporters</h1>
-        <MarqueeComp/>
-        <div className='flex flex-col mt-12  justify-center gap-8'>
-            <div className='flex flex-col gap-4 w-[40vw]'>
-        <h1 className='text-black font-semibold md:text-3xl text-lg '>Introducing the Crypto Shophe Debit Card</h1>
-<p className='text-[#6B6F93] font-[400] text-[16px]'>Seamlessly Convert Your Crypto into Cash Anywhere.</p>
+      <h1 className='text-black font-semibold md:text-3xl text-lg text-center'>Partners and Supporters</h1>
+      <MarqueeComp />
+      <div className='flex  mt-12 justify-center gap-4'>
+        <div className='flex flex-col gap-6 w-[40vw]'>
+          <h1 className='text-black font-semibold md:text-3xl text-lg'>Introducing the Crypto Shophe Debit Card</h1>
+          <p className='text-[#6B6F93] font-[400] text-[16px]'>Seamlessly Convert Your Crypto into Cash Anywhere.</p>
+        
+        <div className='flex gap-8'>
+          <div className='flex flex-col text-[#6B6F93] font-[400] text-[16px]'>
+            <div className='flex items-center'>
+              <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
+              <p>Multi-Currency support</p>
             </div>
-<div className='flex gap-8'>
-    <div className='text-[#6B6F93] font-[400] text-[16px]'>
-<p>Multi-Currency support</p>
-<p>CSHOP Token Integration</p>
-<p>Look Exhange Rates</p>
-
-    </div>
-    <div className='text-[#6B6F93] font-[400] text-[16px]'>
-<p>Virtual and Physical Card</p>
-<p>Crypto Currency Security</p>
-<p>And much more</p>
-
-    </div>
-</div>
+            <div className='flex items-center'>
+              <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
+              <p>CSHOP Token Integration</p>
+            </div>
+            <div className='flex items-center'>
+              <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
+              <p>Look Exchange Rates</p>
+            </div>
+          </div>
+          <div className='flex flex-col text-[#6B6F93] font-[400] text-[16px]'>
+            <div className='flex items-center'>
+              <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
+              <p>Virtual and Physical Card</p>
+            </div>
+            <div className='flex items-center'>
+              <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
+              <p>Crypto Currency Security</p>
+            </div>
+            <div className='flex items-center'>
+              <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
+              <p>And much more</p>
+            </div>
+          </div>
         </div>
+        </div>
+       
+        <Image alt='MasterCard' src="/images/homepage/partners/mastercard.png" height={524} width={524} className='h-auto w-[50vw] absolute top-0'></Image>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;

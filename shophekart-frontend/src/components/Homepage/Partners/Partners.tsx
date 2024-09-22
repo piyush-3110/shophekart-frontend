28cc295b799ced907c668d0da44d39db6d23a93c
Piyush-Das <piyushdas3110@gmail.com>
2024-09-22 20:31:10 +0530

    added gradient button to partner component

--- shophekart-frontend/src/components/Homepage/Partners/Partners.tsx
+++ shophekart-frontend/src/components/Homepage/Partners/Partners.tsx
@@ -11,12 +11,12 @@ const Partners = () => {
 
       </div>
       <div className='flex  mt-8 justify-center gap-4'>
-        <div className='flex flex-col gap-6 mt-6 w-[40vw]'>
+        <div className='flex flex-col pl-12 gap-6 mt-6 w-[40vw]'>
           <h1 className='text-black font-semibold md:text-3xl text-lg'>Introducing the Crypto Shophe Debit Card</h1>
           <p className='text-[#6B6F93] font-[400] text-[16px]'>Seamlessly Convert Your Crypto into Cash Anywhere.</p>
         
         <div className='flex gap-8'>
-          <div className='flex flex-col text-[#6B6F93] font-[400] text-[16px]'>
+          <div className='flex flex-col  text-[#6B6F93] font-[400] text-[16px]'>
             <div className='flex items-center'>
               <Image src="/images/homepage/partners/ellipse.png" alt="Logo" width={16} height={16} className="h-3 w-3 mr-2" />
               <p>Multi-Currency support</p>
@@ -45,6 +45,7 @@ const Partners = () => {
             </div>
           </div>
         </div>
+        <button className='gradient-button w-[10rem]'>Learn More</button>
         </div>
    
        <Image alt='MasterCard' src="/images/homepage/partners/mastercard.png" height={524} width={524} className='h-auto w-[50vw] translate-x-[10%] -translate-y-[20%]'></Image>

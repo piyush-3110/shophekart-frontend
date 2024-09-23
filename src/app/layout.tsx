04bc7983be044106995ffb4812803cf8e83201ac
Umar Khan <khanumar0108@gmail.com>
2024-09-23 17:08:49 +0530

    refactor: add navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import {DM_Sans} from "next/font/google"
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
- const dm = DM_Sans({
-  subsets:['latin'],
-  weight:['400','500','700'],
- 
- })
-
+import FloatingNavbar from "@/components/Navbar/FloatingNavbar";
+const dm = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata: Metadata = {
   title: "ShophEkart",
   description: "Your one-stop crypto shop",
- 
 };
 
 export default function RootLayout({
@@ -22,11 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="./images/shared/favicon.png" />
+        <link rel="icon" href="./images/shared/favicon.png" />
       </head>
-      <body
-        className={`${dm.className} antialiased`}
-      >
+      <body className={`${dm.className} antialiased`}>
+        <FloatingNavbar />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import FloatingNavbar from '@/components/Homepage/Hero/Navbar/FloatingNavbar'
-import { Hero } from '@/components/Homepage/Hero/Main/Hero'
-import { Tokenomics } from '@/components/Homepage/Tokenomics/Tokenomics'
-import { Roadmap } from '@/components/Homepage/Roadmap/Roadmap'
-import Partners from '@/components/Homepage/Partners/Partners'
+import { Hero } from "@/components/Homepage/Hero/Main/Hero";
+import { Tokenomics } from "@/components/Homepage/Tokenomics/Tokenomics";
+import { Roadmap } from "@/components/Homepage/Roadmap/Roadmap";
+import Partners from "@/components/Homepage/Partners/Partners";
 const page = () => {
   return (
-    <div className='bg-[#f1f4ff]  '>
-      <FloatingNavbar/>
-      <Hero/>
-      <Tokenomics/>
-      <Roadmap/>
-      <Partners/>
+    <div className="bg-[#f1f4ff]  ">
+      <Hero />
+      <Tokenomics />
+      <Roadmap />
+      <Partners />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;

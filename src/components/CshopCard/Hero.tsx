44d29cdf72d0c7ee80a766e1b36f11a3b1233bf2
Piyush-Das <piyushdas3110@gmail.com>
2024-09-27 01:20:55 +0530

    added content to the hero section of cshop card including gradient texts

--- src/components/CshopCard/Hero.tsx
+++ src/components/CshopCard/Hero.tsx
@@ -3,10 +3,14 @@ import React from 'react'
 export const Hero = () => {
   return (
     <div className='min-h-[100vh] flex items-center justify-center gap-12 '>
-        <div className='bg-red-100'>
+        <div className='bg-red-100 w-[50vw]'>
             {/* Div for content */}
             <h1 className='text-wrap w-full text-[23px] md:text-[44px] font-semibold'>CSHOP MasterCard: 
-            The <span className='gradient-text !text-[22px] md:!text-[44px] whitespace-nowrap'>Future of Convenient </span>Cryptocurrency Spending</h1>
+            The <span className='gradient-text !text-[22px] md:!text-[44px] '>Future of Convenient </span>Cryptocurrency Spending</h1>
+            <button className='gradient-button my-8'>Get Your Card</button>
+        </div>
+        <div className='w-[40vw] bg-red-400 h-[70vh]'>
+
         </div>
 {/* Div for mastercard */}
     </div>

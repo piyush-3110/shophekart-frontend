082f5f765eaacf4cf12ad43bbb2b12da829f43f2
Piyush-Das <piyushdas3110@gmail.com>
2024-09-27 01:27:27 +0530

    added mastercard animation in hero section

--- src/components/CshopCard/Hero.tsx
+++ src/components/CshopCard/Hero.tsx
@@ -1,16 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const Hero = () => {
   return (
-    <div className='min-h-[100vh] flex items-center justify-center gap-12 '>
-        <div className='bg-red-100 w-[50vw]'>
+    <div className='min-h-[90vh] flex items-center justify-center gap-28 '>
+        <div className=' w-[40vw]'>
             {/* Div for content */}
-            <h1 className='text-wrap w-full text-[23px] md:text-[44px] font-semibold'>CSHOP MasterCard: 
-            The <span className='gradient-text !text-[22px] md:!text-[44px] '>Future of Convenient </span>Cryptocurrency Spending</h1>
+            <h1 className='text-wrap w-full text-[23px] md:text-[40px] font-semibold'>CSHOP MasterCard: 
+            The <span className='gradient-text !text-[22px] md:!text-[40px] '>Future of Convenient </span>Cryptocurrency Spending</h1>
             <button className='gradient-button my-8'>Get Your Card</button>
         </div>
-        <div className='w-[40vw] bg-red-400 h-[70vh]'>
-
+        <div className='lg:block hidden mb-16 z-[1] relative mt-8 mr-8'>
+          <Image
+            alt='MasterCard'
+            src='/images/homepage/Partners/mastercard2.png'
+            height={524}
+            width={524}
+            className='h-[50vh] w-auto rotate-left'
+          />
+          <Image
+            alt='MasterCard'
+            src='/images/homepage/Partners/mastercard1.png'
+            height={524}
+            width={524}
+            className='h-[40vh] w-auto absolute z-[1] top-0 -right-10 rotate-right'
+          />
         </div>
 {/* Div for mastercard */}
     </div>

09e795cd033fc4d3532e1ee784f7fb464071fa8a
Piyush-Das <piyushdas3110@gmail.com>
2024-09-27 01:15:06 +0530

    added cShop card pages

--- src/components/CshopCard/Hero.tsx
+++ src/components/CshopCard/Hero.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Hero = () => {
   return (
     <div className='min-h-[100vh] flex items-center justify-center gap-12 '>
-        <div>
+        <div className='bg-red-100'>
             {/* Div for content */}
             <h1 className='text-wrap w-full text-[23px] md:text-[44px] font-semibold'>CSHOP MasterCard: 
-            The Future of Convenient Cryptocurrency Spending</h1>
+            The <span className='gradient-text !text-[22px] md:!text-[44px] whitespace-nowrap'>Future of Convenient </span>Cryptocurrency Spending</h1>
         </div>
 {/* Div for mastercard */}
     </div>

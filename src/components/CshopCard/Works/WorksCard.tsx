be5b952096dd8c05531cce7e29bb080b6cd1f7e6
Piyush-Das <piyushdas3110@gmail.com>
2024-09-28 00:39:03 +0530

    made the conten of workscard reusable

--- src/components/CshopCard/Works/WorksCard.tsx
+++ src/components/CshopCard/Works/WorksCard.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const WorksCard = () => {
+interface WorksCardProps {
+  heading: string;
+  content: string;
+}
+
+const WorksCard: React.FC<WorksCardProps> = ({ heading, content }) => {
   return (
     <div className="relative text-white w-[30vw] h-fit py-6 px-7 card-border-gradient">
       {/* Content */}
       <div className="relative z-10">
-        <h2 className="text-md font-[700]   mb-4">Top Up Your Card</h2>
-        <p className='text-sm text-[#6B6F93]'>
-        Before making purchases, top up your card with any of the supported cryptocurrencies, such as USDT, USDC, or CSHOP tokens. Simply transfer your crypto from your wallet to your card through the Shophekart platform.
-
-You can top up as frequently as needed, and all funds will be available for instant use.
-
+        <h2 className="text-md font-[700] mb-4">{heading}</h2>
+        <p className="text-sm text-[#6B6F93]">
+          {content}
         </p>
       </div>
-
-     
     </div>
   );
 };

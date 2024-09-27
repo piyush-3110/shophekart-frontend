839299dde579664246ba11b4a57078c84120ac31
Piyush-Das <piyushdas3110@gmail.com>
2024-09-28 01:03:57 +0530

    added central line and logo of timeline

--- src/components/CshopCard/Works/Works.tsx
+++ src/components/CshopCard/Works/Works.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import  WorksCard  from './WorksCard'
+
+import Timeline from './Timeline'
 
 export const Works = () => {
    
@@ -9,10 +10,11 @@ export const Works = () => {
            <h1 className="text-white font-semibold py-8 md:text-3xl text-lg text-center">
        How It Works
       </h1>
-      <WorksCard
+      {/* <WorksCard
         heading="Spend Instantly"
         content="Once your card is topped up, you can start spending immediately with any supported retailer. Enjoy the flexibility of using your cryptocurrency in the real world."
-      />
+      /> */}
+      <Timeline/>
     </div>
   )
 }

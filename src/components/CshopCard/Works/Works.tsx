fb65e773f68d8c54e669a6e4e8c11a9714842ecf
Piyush-Das <piyushdas3110@gmail.com>
2024-09-27 23:32:53 +0530

    added How it worrks component in the cshop card page

---
+++ src/components/CshopCard/Works/Works.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Works = () => {
+  return (
+    <div className='min-h-[100vh] bg-black'>
+           <h1 className="text-white font-semibold py-8 md:text-3xl text-lg text-center">
+       How It Works
+      </h1>
+    </div>
+  )
+}

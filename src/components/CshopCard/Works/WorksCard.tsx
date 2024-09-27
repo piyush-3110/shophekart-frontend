dcbc34ef1510fbca2d1a56ba43ea78dc9a9860d8
Piyush-Das <piyushdas3110@gmail.com>
2024-09-28 00:37:48 +0530

    added workscard

--- src/components/CshopCard/Works/WorksCard.tsx
+++ src/components/CshopCard/Works/WorksCard.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 const WorksCard = () => {
   return (
-    <div className="relative text-white h-fit py-6 px-4 card-border-gradient">
+    <div className="relative text-white w-[30vw] h-fit py-6 px-7 card-border-gradient">
       {/* Content */}
       <div className="relative z-10">
-        <h2 className="text-xl font-semibold mb-4">WorksCard Component</h2>
-        <p>
-          This is a placeholder content for the WorksCard component. The border is styled with a linear gradient that covers part of the top-left and bottom-right edges.
+        <h2 className="text-md font-[700]   mb-4">Top Up Your Card</h2>
+        <p className='text-sm text-[#6B6F93]'>
+        Before making purchases, top up your card with any of the supported cryptocurrencies, such as USDT, USDC, or CSHOP tokens. Simply transfer your crypto from your wallet to your card through the Shophekart platform.
+
+You can top up as frequently as needed, and all funds will be available for instant use.
+
         </p>
       </div>
 

edc89657f7b78abbe14ff6a96a790e01d907301b
Umar Khan <khanumar0108@gmail.com>
2024-09-27 15:06:39 +0530

    Merge branch 'main' of https://github.com/piyush-3110/shophekart-frontend

---
+++ src/app/cshopCard/page.tsx
@@ -0,0 +1,13 @@
+import { Features } from "@/components/CshopCard/Features/Features"
+import { Hero } from "@/components/CshopCard/Hero"
+
+const page = () => {
+  return (
+    <div>
+        <Hero/>
+        <Features/>
+    </div>
+  )
+}
+
+export default page

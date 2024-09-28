d550c17d098d18a1c6ba302e6eae12dc2cd12198
Piyush-Das <piyushdas3110@gmail.com>
2024-09-29 02:11:20 +0530

    fixed feature grid for medium screens

--- src/components/CshopCard/Features/Features.tsx
+++ src/components/CshopCard/Features/Features.tsx
@@ -32,7 +32,18 @@ export const Features = () => {
           border-bottom: 1px solid #6b7280;
           border-right: 1px solid #6b7280;
         }
- @media (min-width: 768px) {
+           .card-border-gradient {
+          border: 1px solid transparent;
+          background: linear-gradient(#101317, #101317) padding-box,
+            linear-gradient(
+              117.79deg,
+              rgba(2, 171, 255, 0.8) -3.21%,
+              rgba(255, 255, 255, 0) 30.3%,
+              rgba(255, 255, 255, 0) 57.52%,
+              rgba(2, 47, 208, 0.8) 101.5%
+            ) border-box;
+        }
+ @media (min-width: 820px) {
           .card:nth-child(4n) {
             border-right: 0;
           }
@@ -45,42 +56,10 @@ export const Features = () => {
         }
 
         /* Medium screens (md) */
-        @media (max-width: 768px) {
-          .card {
-            border-bottom: 1px solid #6b7280;
-            border-right: 1px solid #6b7280;
-            border-top: 1px solid #6b7280;
-            border-left: 1px solid #6b7280;
-          }
-
-          /* Remove left border for cards 1, 4, and 7 */
-          #card1,
-          #card4,
-          #card7 {
-            border-left: 0;
-          }
-
-          /* Remove bottom border for cards 7 and 8 */
-         
-
-          .card:nth-child(3n) {
-            border-right: 0;
-          }
-          .card:nth-last-child(-n + 3) {
-            border-bottom: 0;
-          }
-          .card:nth-child(-n + 3) {
-            border-top: 0;
-          }
-             #card7,
-          #card8 {
-            border-bottom: 0;
-            border-right:1px solid #6b7280;
-          }
-        }
+       
 
         /* Small screens (sm) */
-        @media (max-width: 640px) {
+        @media (max-width: 820px) {
          .card{
          border:0;
          }
@@ -94,17 +73,7 @@ export const Features = () => {
         /* Large screens */
        
         /* Gradient border */
-        .card-border-gradient {
-          border: 1px solid transparent;
-          background: linear-gradient(#101317, #101317) padding-box,
-            linear-gradient(
-              117.79deg,
-              rgba(2, 171, 255, 0.8) -3.21%,
-              rgba(255, 255, 255, 0) 30.3%,
-              rgba(255, 255, 255, 0) 57.52%,
-              rgba(2, 47, 208, 0.8) 101.5%
-            ) border-box;
-        }
+       
       `}</style>
     </div>
   );

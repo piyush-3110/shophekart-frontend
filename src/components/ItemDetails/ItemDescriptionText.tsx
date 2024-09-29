09d0738c917ec743c4654467ec03e80e4a522b0c
Piyush-Das <piyushdas3110@gmail.com>
2024-09-30 00:28:44 +0530

    added Item Description text

---
+++ src/components/ItemDetails/ItemDescriptionText.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItemDescriptionText = () => {
+  return (
+    <p className="text-sm font-[400] text-[#6B6F93]">
+      Made by new technic corduroy, sturdier than normal fabric. Super soft
+      and lightweight. Water resistance and reinforced bottom. Not only the
+      durability of the product, but also maintains its lightness.
+    </p>
+  );
+};
+
+export default ItemDescriptionText;

65d6aaf452f5b40685ca9b099e621a2bbc8ee526
Piyush-Das <piyushdas3110@gmail.com>
2024-09-30 15:36:55 +0530

    added linear gradient

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Profile = () => {
   return (
-    <div></div>
+    <div className='w-full'>
+        <div className='w-full h-[13rem] bg-gradient-to-r'style={{
+          background: 'linear-gradient(to right, #25a5f6, #5aa1f6, #aabff9)',
+        }}>
+
+        </div>
+    </div>
   )
 }

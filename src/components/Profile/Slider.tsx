167d6d79f6cfe9657f172caf8c93263372965529
Piyush-Das <piyushdas3110@gmail.com>
2024-10-01 15:24:36 +0530

    created profile card

---
+++ src/components/Profile/Slider.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Slider = () => {
+  return (
+    <div>
+        <div className='text-lg font-[700] pt-32 px-32 text-[#6B6F93] flex gap-6 items-center'>
+            <p>Items</p>
+            <p>Items For Sale</p>
+            <p>Comments</p>
+
+        </div>
+    </div>
+  )
+}

6e7360ec2e287531c25529f75933c5b9f1d1c501
sakshat kumar <sakshatkumar@sakshats-MacBook-Air.local>
2024-10-20 17:06:21 +0530

    add form

---
+++ src/app/form/page.tsx
@@ -0,0 +1,9 @@
+import ProductForm from '../../components/Form/ProductForm';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <ProductForm />
+    </div>
+  );
+}

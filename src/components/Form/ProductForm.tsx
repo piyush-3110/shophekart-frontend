a902229c3b50864de88fd1d167d831ccdbb2932c
Umar <khanumar0108@gmail.com>
2024-10-21 17:58:17 +0530

    feat: Make it use HttpRequestService and fix eslint errors

--- src/components/Form/ProductForm.tsx
+++ src/components/Form/ProductForm.tsx
@@ -1,35 +1,39 @@
-"use client"
-import React, { useState } from 'react';
-import UploadImage from './UploadImage';
-import axios from 'axios';
-import RichTextEditor from './RichTextArea';
-import InputField from './InputField';
-import TextArea from './TextArea';
-import Button from './Button';
-import { toast } from 'react-toastify';
-import ToastNotification from './ToastNotification';
-import Loader from './Loader';
-import CategorySelect from './CategorySelect'; // Import CategorySelect
-import SelectField from './SelectField';
+"use client";
+import React, { useState } from "react";
+import UploadImage from "./UploadImage";
+import RichTextEditor from "./RichTextArea";
+import InputField from "./InputField";
+import TextArea from "./TextArea";
+import Button from "./Button";
+import { toast } from "react-toastify";
+import ToastNotification from "./ToastNotification";
+import Loader from "./Loader";
+import CategorySelect from "./CategorySelect";
+import SelectField from "./SelectField";
+import { HttpRequestService } from "@/services";
 
 const ProductForm = () => {
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    productAddress: '',
-    details: '',
-    category: '', // Will hold category ID
-    currencyType: '',
-    stock: '',
-    price: '',
-    shippingCharges: '',
-    shippingType: '',
+    name: "",
+    description: "",
+    productAddress: "",
+    details: "",
+    category: "", // Will hold category ID
+    currencyType: "",
+    stock: "",
+    price: "",
+    shippingCharges: "",
+    shippingType: "",
   });
 
   const [selectedImages, setSelectedImages] = useState<File[]>([]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
@@ -51,51 +55,54 @@ const ProductForm = () => {
     setLoading(true);
 
     const formDataToSubmit = new FormData();
-    formDataToSubmit.append('name', formData.name);
-    formDataToSubmit.append('description', formData.description);
-    formDataToSubmit.append('productAddress', formData.productAddress);
-    formDataToSubmit.append('details', formData.details);
-    formDataToSubmit.append('category', formData.category); // Send category ID to backend
-    formDataToSubmit.append('currencyType', formData.currencyType);
-    formDataToSubmit.append('stock', formData.stock);
-    formDataToSubmit.append('price', formData.price);
-    formDataToSubmit.append('shippingCharges', formData.shippingCharges);
-    formDataToSubmit.append('shippingType', formData.shippingType);
-    formDataToSubmit.append('productIdOnChain', '123456'); // Example product ID on chain
-    formDataToSubmit.append('sellerId', '64a92b4f8f3b74a0acbfcfc1'); // Example seller ID
-    formDataToSubmit.append('currencyAddress', 'dshsjkahbsahsagdjhfsad');
+    formDataToSubmit.append("name", formData.name);
+    formDataToSubmit.append("description", formData.description);
+    formDataToSubmit.append("productAddress", formData.productAddress);
+    formDataToSubmit.append("details", formData.details);
+    formDataToSubmit.append("category", formData.category); // Send category ID to backend
+    formDataToSubmit.append("currencyType", formData.currencyType);
+    formDataToSubmit.append("stock", formData.stock);
+    formDataToSubmit.append("price", formData.price);
+    formDataToSubmit.append("shippingCharges", formData.shippingCharges);
+    formDataToSubmit.append("shippingType", formData.shippingType);
+    formDataToSubmit.append("productIdOnChain", "123456"); // Example product ID on chain
+    formDataToSubmit.append("sellerId", "64a92b4f8f3b74a0acbfcfc1"); // Example seller ID
+    formDataToSubmit.append("currencyAddress", "dshsjkahbsahsagdjhfsad");
 
     selectedImages.forEach((file) => {
-      formDataToSubmit.append('images', file);
+      formDataToSubmit.append("images", file);
     });
 
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/fixedProduct/create', formDataToSubmit, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+      await HttpRequestService.postApi(
+        `/fixedProduct/create`,
+        formDataToSubmit,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
       setLoading(false);
-      toast.success('Product Created Successfully');
+      toast.success("Product Created Successfully");
 
       // Clear the form and selected images
       setFormData({
-        name: '',
-        description: '',
-        productAddress: '',
-        details: '',
-        category: '',
-        currencyType: '',
-        stock: '',
-        price: '',
-        shippingCharges: '',
-        shippingType: '',
+        name: "",
+        description: "",
+        productAddress: "",
+        details: "",
+        category: "",
+        currencyType: "",
+        stock: "",
+        price: "",
+        shippingCharges: "",
+        shippingType: "",
       });
       setSelectedImages([]); // Reset selected images
-
     } catch (error) {
       setLoading(false);
-      toast.error("Failed to create product");
+      if (error) toast.error("Failed to create product");
     }
   };
 
@@ -136,13 +143,21 @@ const ProductForm = () => {
           onChange={handleCategoryChange}
         />
         <div>
-          <label className="block text-sm font-medium mb-1">Product media</label>
+          <label className="block text-sm font-medium mb-1">
+            Product media
+          </label>
           <UploadImage onFileSelect={handleFileSelect} />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
           <SelectField
             label="Currency Type"
-            options={['Select Currency Type', 'USDT', 'BNB', 'CSHOP', 'USDC'].map(type => ({ label: type, value: type }))}
+            options={[
+              "Select Currency Type",
+              "USDT",
+              "BNB",
+              "CSHOP",
+              "USDC",
+            ].map((type) => ({ label: type, value: type }))}
             name="currencyType"
             value={formData.currencyType}
             onChange={handleChange}
@@ -151,7 +166,7 @@ const ProductForm = () => {
             label="Stock"
             placeholder="Enter stock"
             name="stock"
-            type='number'
+            type="number"
             value={formData.stock}
             onChange={handleChange}
           />
@@ -159,7 +174,7 @@ const ProductForm = () => {
             label="Price"
             placeholder="Enter price"
             name="price"
-            type='number'
+            type="number"
             value={formData.price}
             onChange={handleChange}
           />
@@ -167,25 +182,29 @@ const ProductForm = () => {
             label="Shipping charges"
             placeholder="Enter Shipping price"
             name="shippingCharges"
-            type='number'
+            type="number"
             value={formData.shippingCharges}
             onChange={handleChange}
           />
           <SelectField
             label="Shipping Type"
-            options={['Select Shipping Type', 'LOCAL', 'GLOBAL'].map(type => ({ label: type, value: type }))}
+            options={["Select Shipping Type", "LOCAL", "GLOBAL"].map(
+              (type) => ({ label: type, value: type })
+            )}
             name="shippingType"
             value={formData.shippingType}
             onChange={handleChange}
           />
         </div>
-        
-        <Button text={loading ? <Loader /> : "Save and publish product"} disabled={loading} />
+
+        <Button
+          text={loading ? <Loader /> : "Save and publish product"}
+          disabled={loading}
+        />
         <ToastNotification />
       </form>
     </div>
   );
 };
-
 
 export default ProductForm;

21f56baebc213a8abf5113e85bf156898071f4ef
Piyush-Das <piyushdas3110@gmail.com>
2024-10-21 22:27:56 +0530

    updated the product

--- src/components/ItemDetails/ItemInfoHeader.tsx
+++ src/components/ItemDetails/ItemInfoHeader.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import Rating from "./Rating";
 import Image from "next/image";
@@ -18,8 +19,8 @@ const ItemInfoHeader: React.FC<ItemInfoHeaderProps> = ({ name, stock }) => {
         <Rating ratingValue={3.5} />
         <h1 className="text-[#6B6F93] font-[700] text-sm">1980</h1>
         <div className="w-[1px] bg-[#6B6F93] h-4"></div>
-        <h1 className="text-[#160041] font-[700] text-md">Stock: {stock}</h1> {/* Display stock dynamically */}
-        <div className="w-[1px] bg-[#160041] h-4"></div>
+        {/* <h1 className="text-[#160041] font-[700] text-md">Stock: {stock}</h1>  */}
+        {/* <div className="w-[1px] bg-[#160041] h-4"></div> */}
         <div className="flex gap-1 items-center">
           <Image
             src="/images/itemDetails/contact.png"

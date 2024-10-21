79804c1ee4c66d0c4aad4903bee24440f899bf40
Umar <khanumar0108@gmail.com>
2024-10-21 17:59:00 +0530

    fix: Casing issue in SVG attributes.

--- src/icons/CartIcon.tsx
+++ src/icons/CartIcon.tsx
@@ -42,32 +42,32 @@ const CartIcon = ({
       <path
         d="M8.125 8.30905V7.25822C8.125 4.82072 10.0858 2.42655 12.5233 2.19905C15.4267 1.91739 17.875 4.20322 17.875 7.05239V8.54739"
         stroke={strokeColor}
-        stroke-width={strokeWidth}
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth={strokeWidth}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.74957 23.8334H16.2496C20.6046 23.8334 21.3846 22.0892 21.6121 19.9659L22.4246 13.4659C22.7171 10.8226 21.9587 8.66675 17.3329 8.66675H8.66624C4.0404 8.66675 3.28207 10.8226 3.57457 13.4659L4.38707 19.9659C4.61457 22.0892 5.39457 23.8334 9.74957 23.8334Z"
         stroke={strokeColor}
-        stroke-width={strokeWidth + 0.5}
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth={strokeWidth + 0.5}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M16.7867 12.9999H16.7964"
         stroke={strokeColor}
-        stroke-width={strokeWidth + 0.5}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth={strokeWidth + 0.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.20271 12.9999H9.21244"
         stroke={strokeColor}
-        stroke-width={strokeWidth + 0.5}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth={strokeWidth + 0.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );

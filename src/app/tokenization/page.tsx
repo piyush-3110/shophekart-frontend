7c7fb2fa91691af6a03a783f87c5eef06ae9eb02
Piyush-Das <piyushdas3110@gmail.com>
2024-10-22 16:53:01 +0530

    Created hero section for tokenization

--- src/app/tokenization/page.tsx
+++ src/app/tokenization/page.tsx
@@ -2,12 +2,16 @@
 import React from 'react'
 
 import Footer from '@/components/Footer/Footer'
-import Hero from '@/components/tokenization/Hero'
+import { Hero } from '@/components/tokenization/Hero';
+import { Tokenize } from '@/components/tokenization/Tokenize';
+
 
 const page = () => {
   return (
     <div>
+     
      <Hero/>
+     <Tokenize/>
        <Footer/>
     </div>
   )

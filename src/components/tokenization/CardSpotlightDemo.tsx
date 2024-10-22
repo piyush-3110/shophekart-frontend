18cdc8d557f4a8f449e3ce04d896e36a99c52793
Piyush-Das <piyushdas3110@gmail.com>
2024-10-22 18:15:18 +0530

    added tokenizer cards

--- src/components/tokenization/CardSpotlightDemo.tsx
+++ src/components/tokenization/CardSpotlightDemo.tsx
@@ -7,7 +7,7 @@ interface CardSpotlightDemoProps {
 
 export function CardSpotlightDemo({ heading, description }: CardSpotlightDemoProps) {
   return (
-    <CardSpotlight className="h-96 w-96">
+    <CardSpotlight className="h-fit w-fit px-4 py-6">
       <p className="text-xl font-bold relative z-20 mt-2 text-white">
         {heading}
       </p>
--- src/components/tokenization/Tokenize.tsx
+++ src/components/tokenization/Tokenize.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useRef } from 'react';
 import lottie from 'lottie-web';
+import { CardSpotlightDemo } from './CardSpotlightDemo';
 
 export const Tokenize = () => {
     const animationContainer = useRef<HTMLDivElement>(null);
@@ -23,10 +24,37 @@ export const Tokenize = () => {
 
     return (
         <div 
-            className="px-8 md:px-12 py-8 min-h-[100vh] bg-[url('/images/tokenization/bg.png')] bg-cover bg-center flex gap-6"
+            className="px-8 md:px-12 py-8 min-h-[100vh] bg-[#000] bg-cover bg-center "
         >
-            
-            <div ref={animationContainer} className="w-[10rem]"></div>
+            <div className='flex gap-6 items-center '>
+
+        
+            <div ref={animationContainer} className="w-[90vw] lg:h-[80vh] lg:w-[40vw] "></div>
+            <div className='grid grid-cols-1 gap-3 md:grid-cols-2 w-[50vw]'>
+            <CardSpotlightDemo 
+  heading="Luxury Cars" 
+  description="Tokenize classic and high-end vehicles, enabling owners to sell either the entire car or fractional shares. This will make rare and vintage automobiles more accessible to a broader range of investors and collectors.
+" 
+/>
+<CardSpotlightDemo 
+  heading="Watches" 
+  description="High-value timepieces will be tokenized to verify authenticity and secure ownership. Through NFTs, collectors can trade fractions of luxury watches, bringing greater flexibility to the market for prestigious watches.
+" 
+/>
+<CardSpotlightDemo 
+  heading="Fine Art" 
+  description="Tokenize valuable artworks with digital certificates of authenticity through NFTs. Art enthusiasts will be able to invest in shares of fine art, creating a new frontier for fractional art ownership.
+" 
+/>
+<CardSpotlightDemo 
+  heading="Tokenized Collectibles" 
+  description="Rare items like limited-edition sneakers, designer fashion, and unique collectibles will also be available for tokenization, allowing a vibrant marketplace for these in-demand products." 
+/>
+
+
+
+            </div>
+            </div>
         </div>
     );
 };

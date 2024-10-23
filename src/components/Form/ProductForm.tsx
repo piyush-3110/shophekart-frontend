96cc8214506ea01d194afcf138084a05c7fd5b4d
Umar <khanumar0108@gmail.com>
2024-10-23 20:57:21 +0530

    feat: Integrate smart contract function to create product

--- src/components/Form/ProductForm.tsx
+++ src/components/Form/ProductForm.tsx
@@ -5,32 +5,20 @@ import RichTextEditor from "./RichTextArea";
 import InputField from "./InputField";
 import TextArea from "./TextArea";
 import Button from "./Button";
-import { toast } from "react-toastify";
 import ToastNotification from "./ToastNotification";
 import Loader from "./Loader";
 import CategorySelect from "./CategorySelect";
 import SelectField from "./SelectField";
-import { HttpRequestService, PinataService } from "@/services";
-import { useWriteContract } from "wagmi";
-import { config } from "@/config";
-import CONTRACT_CONFIG from "@/constants/contractConfig";
-import { parseEther } from "viem";
-import TOKEN_ADDRESS from "@/constants/tokenAddress";
-import { IProduct } from "@/types";
-import { envConfig } from "@/config/envConfig";
-import axios from "axios";
 import { useUserStore } from "@/store/userStore";
-import { waitForTransactionReceipt } from "@wagmi/core";
-import { useWatchContractEvent } from "wagmi";
-import { bscTestnet } from "viem/chains";
-import { MARKETPLACE_ABI } from "@/constants/ABI";
+import { useCreateProduct } from "@/hooks";
+import { TCreateProductData } from "@/types";
 
 const ProductForm = () => {
   const [loading, setLoading] = useState(false);
 
-  const [marketItemId, setMarketItemId] = useState<bigint | null>(null);
+  const { user } = useUserStore();
 
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<TCreateProductData>({
     name: "",
     description: "",
     productAddress: "",
@@ -39,40 +27,18 @@ const ProductForm = () => {
     currencyType: "CSHOP",
     stock: "",
     price: "",
-    shippingCharges: "",
-    shippingType: "",
+    shippingCharge: "",
+    shippingType: "LOCAL",
     shippingDuration: "",
+    images: [],
+    sellerId: user?._id ?? "0x0000000000000000000000000000000000000000",
   });
 
   const [selectedImages, setSelectedImages] = useState<File[]>([]);
-  const { writeContractAsync, isPending } = useWriteContract({
-    config,
-  });
 
-  useWatchContractEvent({
-    // ...CONTRACT_CONFIG.marketplace,
-    address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
-    abi: MARKETPLACE_ABI,
-    eventName: "MarketItemCreated",
-    batch: false,
-    onLogs(logs) {
-      setMarketItemId(logs[0].args?.marketItemId.toString());
-    },
-    chainId: bscTestnet.id,
-    onError(error) {
-      console.log(error);
-    },
-    config,
-  });
-
-  const { user } = useUserStore();
-
-  const currencyTypeAddresses = {
-    USDT: TOKEN_ADDRESS.usdt,
-    BNB: TOKEN_ADDRESS.bnb,
-    CSHOP: TOKEN_ADDRESS.cshop,
-    USDC: TOKEN_ADDRESS.usdc,
-  };
+  const { mutateAsync } = useCreateProduct(
+    user?.walletAddress ?? "0x0000000000000000000000000000000000000000"
+  );
 
   const handleChange = (
     e: React.ChangeEvent<
@@ -98,92 +64,51 @@ const ProductForm = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-
-    const formDataToSubmit = new FormData();
-    formDataToSubmit.append("name", formData.name);
-    formDataToSubmit.append("description", formData.description);
-    formDataToSubmit.append("productAddress", formData.productAddress);
-    formDataToSubmit.append("details", formData.details);
-    formDataToSubmit.append("category", formData.category); // Send category ID to backend
-    formDataToSubmit.append("currencyType", formData.currencyType);
-    formDataToSubmit.append("stock", formData.stock);
-    formDataToSubmit.append("price", formData.price);
-    formDataToSubmit.append("shippingCharges", formData.shippingCharges);
-    formDataToSubmit.append("shippingDuration", formData.shippingDuration);
-    formDataToSubmit.append("shippingType", formData.shippingType);
-    formDataToSubmit.append(
-      "productIdOnChain",
-      marketItemId?.toString() ?? "12345"
-    );
-    formDataToSubmit.append("sellerId", user?._id ?? ""); // Example seller ID
-    formDataToSubmit.append(
-      "currencyAddress",
-      currencyTypeAddresses[
-        formData.currencyType as keyof typeof currencyTypeAddresses
-      ]
-    );
-
-    selectedImages.forEach((file) => {
-      formDataToSubmit.append("images", file);
-    });
-    let product;
     try {
-      const response = await HttpRequestService.postApi<IProduct, FormData>(
-        `/fixedProduct/create`,
-        formDataToSubmit,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const formDataToSubmit = new FormData();
+      formDataToSubmit.append("name", formData.name);
+      formDataToSubmit.append("description", formData.description);
+      formDataToSubmit.append("productAddress", formData.productAddress);
+      formDataToSubmit.append("details", formData.details);
+      formDataToSubmit.append("category", formData.category); // Send category ID to backend
+      formDataToSubmit.append("currencyType", formData.currencyType);
+      formDataToSubmit.append("stock", formData.stock);
+      formDataToSubmit.append("price", formData.price);
+      formDataToSubmit.append("shippingCharge", formData.shippingCharge);
+      formDataToSubmit.append("shippingDuration", formData.shippingDuration);
+      formDataToSubmit.append("shippingType", formData.shippingType);
+      formDataToSubmit.append("sellerId", formData.sellerId); // Example seller ID
+      formDataToSubmit.append("currencyAddress", formData.currencyType);
 
-      const res = await PinataService.uplaodFile({
-        name: response.data.name,
-        description: response.data.description,
-        image: response.data.images[0],
-        attributes: [
-          {
-            trait_type: "Category",
-            value: formData.category,
-          },
-          {
-            trait_type: "Token",
-            value: response.data.currencyType,
-          },
-          {
-            trait_type: "Price",
-            value: response.data.price.toString(),
-          },
-        ],
+      selectedImages.forEach((file) => {
+        formDataToSubmit.append("images", file);
       });
 
-      const TOKEN_URI = PinataService.getFile(res.IpfsHash);
-      if (response.success) {
-        product = response.data;
+      await mutateAsync(formDataToSubmit);
 
-        const hash = await writeContractAsync({
-          ...CONTRACT_CONFIG.marketplace,
-          functionName: "createMarketItem",
-          args: [
-            Number(formData.stock),
-            currencyTypeAddresses[
-              formData.currencyType as keyof typeof currencyTypeAddresses
-            ],
-            parseEther(formData.price),
-            parseEther(formData.shippingCharges),
-            TOKEN_URI,
-          ],
-          value: parseEther("0.001"),
-        });
+      // const formDataToSubmit = new FormData();
+      // Object.entries(formData).forEach(([key, value]) => {
+      //   formDataToSubmit.append(key, value);
+      // });
+      // formDataToSubmit.append("sellerId", user?._id ?? "");
 
-        await waitForTransactionReceipt(config, {
-          hash,
-        });
-      }
+      // const data: TCreateProductData = {
+      //   sellerId: user?._id ?? "",
+      //   currencyType: formData.currencyType,
+      //   name: formData.name,
+      //   description: formData.description,
+      //   details: formData.details,
+      //   images: selectedImages,
+      //   shippingType: formData.shippingType,
+      //   shippingCharge: formData.shippingCharge,
+      //   shippingDuration: formData.shippingDuration,
+      //   category: formData.category,
+      //   productAddress: formData.productAddress,
+      //   price: formData.price,
+      //   stock: formData.stock,
+      // };
 
-      setLoading(false);
-      toast.success("Product Created Successfully");
+      // await mutateAsync(data);
 
       // Clear the form and selected images
       setFormData({
@@ -191,34 +116,22 @@ const ProductForm = () => {
         description: "",
         productAddress: "",
         details: "",
-        category: "",
-        currencyType: "",
+        category: "", // Will hold category ID
+        currencyType: "CSHOP",
         stock: "",
         price: "",
-        shippingCharges: "",
-        shippingType: "",
+        shippingCharge: "",
+        shippingType: "LOCAL",
         shippingDuration: "",
+        images: [],
+        sellerId: "",
       });
+
       setSelectedImages([]); // Reset selected images
     } catch (error) {
-      if (product) {
-        try {
-          await axios.delete(
-            `${envConfig.BACKEND_URL}/product/${product._id}/delete`,
-            {
-              withCredentials: true,
-            }
-          );
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
+      console.log(error);
+    } finally {
       setLoading(false);
-      if (error) {
-        console.log(error);
-        toast.error("Failed to create product");
-      }
     }
   };
 
@@ -295,11 +208,11 @@ const ProductForm = () => {
             onChange={handleChange}
           />
           <InputField
-            label="Shipping charges"
+            label="Shipping charge"
             placeholder="Enter Shipping price"
-            name="shippingCharges"
+            name="shippingCharge"
             type="number"
-            value={formData.shippingCharges}
+            value={formData.shippingCharge}
             onChange={handleChange}
           />
           <SelectField
@@ -322,8 +235,8 @@ const ProductForm = () => {
         </div>
 
         <Button
-          text={loading || isPending ? <Loader /> : "Save and publish product"}
-          disabled={loading || isPending}
+          text={loading ? <Loader /> : "Save and publish product"}
+          disabled={loading}
         />
         <ToastNotification />
       </form>

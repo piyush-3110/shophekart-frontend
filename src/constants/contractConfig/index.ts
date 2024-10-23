bb02865952f0a528ba78a5e135d712f87cf3254a
Umar <khanumar0108@gmail.com>
2024-10-23 15:08:51 +0530

    feat: Add contract configs for usdt, usdc adn chsop

--- src/constants/contractConfig/index.ts
+++ src/constants/contractConfig/index.ts
@@ -1,12 +1,25 @@
-import { MARKETPLACE_ABI } from "../ABI";
+import { ERC20_ABI, MARKETPLACE_ABI } from "../ABI";
+import TOKEN_ADDRESS from "../tokenAddress";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type TContractConfig = Record<string, { address: `0x${string}`; abi: any }>;
 
 const CONTRACT_CONFIG: TContractConfig = {
   marketplace: {
-    address: "0xD73F011b18bce033ee0981228804F2564B76D454",
+    address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
     abi: MARKETPLACE_ABI,
+  },
+  usdt: {
+    address: TOKEN_ADDRESS.usdt,
+    abi: ERC20_ABI,
+  },
+  usdc: {
+    address: TOKEN_ADDRESS.usdc,
+    abi: ERC20_ABI,
+  },
+  cshop: {
+    address: TOKEN_ADDRESS.cshop,
+    abi: ERC20_ABI,
   },
 };
 

e38f61a0d5214121cc17e2bb2f80656b7bb58b2d
Umar <khanumar0108@gmail.com>
2024-10-23 15:12:29 +0530

    feat: Optimize buy now page and use useQuery

--- src/app/products/buy-now/page.tsx
+++ src/app/products/buy-now/page.tsx
@@ -1,80 +1,70 @@
 "use client";
+
 import BuyCard from "@/components/products/BuyCard";
 import ProductPagination from "@/components/products/shared/productPagination";
 import Link from "next/link";
 
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { HttpRequestService } from "@/services";
 import { IProduct } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const PAGINATION_CONSTANT = 6;
 
 const Page = () => {
-  const router = useRouter();
   const searchParams = useSearchParams();
-  const pathname = usePathname();
-
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   const pageParam = searchParams.get("page");
-  const page = pageParam ? parseInt(pageParam) : 1;
+  const currentPage = pageParam ? parseInt(pageParam) : 1;
 
-  const productsPerPage = 6;
-  const totalPages = Math.ceil(products.length / productsPerPage);
-
-  const startIndex = (page - 1) * productsPerPage;
-  const endIndex = startIndex + productsPerPage;
-  const productList = products.slice(startIndex, endIndex);
-
-  const fetchData = async () => {
-    try {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
       const response = await HttpRequestService.fetchApi<IProduct[]>(
         "/fixedProduct/getAll"
       );
-      setProducts(response.data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    // Prevent pushing the same URL if it's already the correct page
-    if (totalPages > 0) {
-      if (page < 1) {
-        router.push(`${pathname}?page=1`);
-      } else if (page > totalPages) {
-        router.push(`${pathname}?page=${totalPages}`);
-      }
-    }
-  }, [page, pathname, router, totalPages]);
+      return response;
+    },
+  });
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
+  if (error || !data) {
+    return <div>Opps something went wrong...</div>;
+  }
+
+  if (data.data.length < 1) {
+    return (
+      <div>Seems like there are no products available at the moment...</div>
+    );
+  }
+
+  const totalPages =
+    data?.data?.length > 0
+      ? Math.ceil(data?.data.length / PAGINATION_CONSTANT)
+      : 1;
+  const startIndex = (currentPage - 1) * PAGINATION_CONSTANT;
+  const endIndex = startIndex + PAGINATION_CONSTANT;
+  const paginatedProducts = data?.data.slice(startIndex, endIndex);
+
   return (
     <section className="space-y-8 py-8">
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-4">
-        {productList.map((product) => {
-          return (
-            <Link key={product._id} href={`/itemDetails/${product._id}`}>
-              <BuyCard
-                id={product._id}
-                productPrice={product.price}
-                productName={product.name}
-                productImage={product.images[0]}
-                currencyType={product.currencyType}
-              />
-            </Link>
-          );
-        })}
+        {paginatedProducts.map((product) => (
+          <Link key={product._id} href={`/itemDetails/${product._id}`}>
+            <BuyCard
+              id={product._id}
+              productPrice={product.price}
+              productName={product.name}
+              productImage={product.images[0]}
+              currencyType={product.currencyType}
+            />
+          </Link>
+        ))}
       </div>
-      <ProductPagination currentPage={page} totalPages={totalPages} />
+      <ProductPagination currentPage={currentPage} totalPages={totalPages} />
     </section>
   );
 };

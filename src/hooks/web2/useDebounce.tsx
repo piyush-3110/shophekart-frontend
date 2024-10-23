05d593d9b999bf01af2ec97128343c76923c8d9c
Piyush-Das <piyushdas3110@gmail.com>
2024-10-24 01:15:36 +0530

    added usedebounce hook

---
+++ src/hooks/web2/useDebounce.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+// Custom hook to debounce a value (e.g., search term)
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}

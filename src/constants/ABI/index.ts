57fba09764b6674eb77ade83a9d1916ca6acd42f
Umar <khanumar0108@gmail.com>
2024-10-23 15:08:00 +0530

    feat: Add typescript type for TOKEN_ADDRESS

--- src/constants/ABI/index.ts
+++ src/constants/ABI/index.ts
@@ -1,1 +1,2 @@
 export { default as MARKETPLACE_ABI } from "./MARKETPLACE_ABI.json";
+export { default as ERC20_ABI } from "./ERC20.json";
--- src/constants/tokenAddress/index.ts
+++ src/constants/tokenAddress/index.ts
@@ -1,4 +1,13 @@
-const TOKEN_ADDRESS = {
+type TAddress = `0x${string}`;
+
+type TTokenAddress = {
+  bnb: TAddress;
+  cshop: TAddress;
+  usdt: TAddress;
+  usdc: TAddress;
+};
+
+const TOKEN_ADDRESS: TTokenAddress = {
   bnb: "0x0000000000000000000000000000000000000000",
   cshop: "0x47C9CCfb227CdD45cCB4b17C0717CE1025206fB4",
   usdt: "0x1Ba1c8C5E4F2CB9cb2Ee116BeB5f185C59523A35",

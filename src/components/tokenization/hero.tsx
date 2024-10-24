fece7200bb6d6bd6d25c20d79016ea6420458f91
Piyush-Das <piyushdas3110@gmail.com>
2024-10-24 21:20:17 +0530

    solved tokenization error

--- src/components/tokenization/hero.tsx
+++ src/components/tokenization/hero.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
-export default function Hero1() {
+const Hero1=()=> {
   const [textIndex, setTextIndex] = useState(0);
   const text1 = "Coming Soon: Tokenization Framework for";
   const text2 = "High-Value Items";
@@ -72,3 +72,4 @@ export default function Hero1() {
     </div>
   );
 }
+export default  Hero1;

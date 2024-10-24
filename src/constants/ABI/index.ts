c1d3d0be5d980be37837249638ddaf3ad3f8edcf
Umar <khanumar0108@gmail.com>
2024-10-24 12:18:13 +0530

    feat: Add escrow contract config

--- src/constants/ABI/index.ts
+++ src/constants/ABI/index.ts
@@ -1,2 +1,3 @@
 export { default as MARKETPLACE_ABI } from "./MARKETPLACE_ABI.json";
+export { default as ESCROW_ABI } from "./ESCROW_ABI.json";
 export { default as ERC20_ABI } from "./ERC20.json";
--- src/constants/contractConfig/index.ts
+++ src/constants/contractConfig/index.ts
@@ -1,4 +1,4 @@
-import { ERC20_ABI, MARKETPLACE_ABI } from "../ABI";
+import { ERC20_ABI, ESCROW_ABI, MARKETPLACE_ABI } from "../ABI";
 import TOKEN_ADDRESS from "../tokenAddress";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -8,6 +8,10 @@ const CONTRACT_CONFIG: TContractConfig = {
   marketplace: {
     address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
     abi: MARKETPLACE_ABI,
+  },
+  escrow: {
+    address: "0x3F5291bfd9cD585F1f02b116f6Eea7c9F6bC83cC",
+    abi: ESCROW_ABI,
   },
   usdt: {
     address: TOKEN_ADDRESS.usdt,

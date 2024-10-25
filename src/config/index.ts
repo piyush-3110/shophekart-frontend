822c390a09bf41d78d9f829eb5cacdebda2affee
Piyush-Das <piyushdas3110@gmail.com>
2024-10-25 15:45:26 +0530

    updated index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,25 @@
 // config/index.tsx
 
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-
 import { cookieStorage, createStorage, http } from "wagmi";
 import { bsc, bscTestnet } from "wagmi/chains";
 
 // Your Reown Cloud project ID
 export const projectId = "5414a59b644946d2fd01f5710c4784ad";
 
+// Dynamically get the URL based on window.location for client-side, or use a fallback for SSR
+const getOrigin = () => {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+  return "https://shophekart-frontend.vercel.app/"; // Fallback for SSR
+};
+
 // Create a metadata object
 const metadata = {
   name: "Shophekart",
   description: "A crypto ecommerce store",
-  url: "http://localhost:3000", // origin must match your domain & subdomain
+  url: getOrigin(), // Dynamically set based on the window location or fallback
   icons: ["/images/shared/logo.png"],
 };
 

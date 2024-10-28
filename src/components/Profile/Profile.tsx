d117922cc500fdd13cb53f63a04ea4eac6725d27
Umar <khanumar0108@gmail.com>
2024-10-28 18:09:55 +0530

    feat: Add check for user authorization

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import { ProfileCard } from './ProfileCard'
-import { Slider } from './Slider'
+"use client";
+
+import React from "react";
+import { ProfileCard } from "./ProfileCard";
+import { Slider } from "./Slider";
+import { useUserStore } from "@/store";
+import AccessDeniedMessage from "../shared/AccessDeniedMessage";
 
 export const Profile = () => {
-  return (
-    <div className='w-full pb-12'>
-      <div className='w-full  h-[14rem] bg-gradient-to-r' style={{
-        background: 'linear-gradient(to right, #25a5f6, #5aa1f6, #aabff9)',
-      }}>
-        <div className='transform translate-y-[20%] md:translate-y-[25%] '>
-          <ProfileCard />
+  const { user } = useUserStore();
 
+  if (!user || !user._id || !user.walletAddress) return <AccessDeniedMessage />;
+
+  return (
+    <div className="w-full pb-12">
+      <div
+        className="w-full  h-[14rem] bg-gradient-to-r"
+        style={{
+          background: "linear-gradient(to right, #25a5f6, #5aa1f6, #aabff9)",
+        }}
+      >
+        <div className="transform translate-y-[20%] md:translate-y-[25%] ">
+          <ProfileCard
+            walletAddress={user.walletAddress}
+            trustScore={user.trustScore}
+          />
         </div>
         <Slider />
       </div>
     </div>
-  )
-}
+  );
+};

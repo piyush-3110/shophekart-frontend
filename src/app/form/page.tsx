d213d5df44e34c16a6b8c7b9493082ae701d6ee3
Piyush-Das <piyushdas3110@gmail.com>
2024-10-28 13:43:10 +0530

    updated checkout form and product page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,5 +1,5 @@
 import ProductForm from "../../components/Form/ProductForm";
-export default function Home() {
+export default function page() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <ProductForm />

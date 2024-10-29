16c6d609324f84c33690874a9d48bd2884122f87
Umar <khanumar0108@gmail.com>
2024-10-29 12:10:26 +0530

    style: Set product image width to full

--- src/components/products/BuyCard.tsx
+++ src/components/products/BuyCard.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 import { FC, useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { Lens } from "@/components/ui/lens";
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
-import { useRouter } from 'next/navigation'; // Import useRouter
+import { useRouter } from "next/navigation"; // Import useRouter
 interface BuyCardProp {
   productPrice: number;
   productName: string;
   productImage: string;
-  id:string;
-  currencyType:string;
+  id: string;
+  currencyType: string;
 }
 
 const BuyCard: FC<BuyCardProp> = ({
@@ -19,39 +19,38 @@ const BuyCard: FC<BuyCardProp> = ({
   productName,
   productImage,
   id,
-  currencyType
+  currencyType,
 }) => {
   const router = useRouter(); // Initialize router
-const[hovering,setHovering]= useState(false)
+  const [hovering, setHovering] = useState(false);
   // Handle navigation to ItemDetail page
   const handleBuyNow = () => {
     const url = `/itemDetails/${id}`;
-    console.log('Navigating to: ', url);
+    console.log("Navigating to: ", url);
     setTimeout(() => {
       router.push(url);
     }, 100);
   };
-  
 
   return (
     <div>
       <div className="w-full relative rounded-sm overflow-hidden max-w-md mx-auto bg-[#F4F6FA] p-4">
-        <Badge className="bg-[#022BFF] hover:bg-[#022BFF]/90 font-normal py-2 px-4 absolute top-4 right-4 z-10 rounded-full">
+        <Badge className="bg-[#022BFF] hover:bg-[#022BFF]/90 font-normal py-2 px-4 absolute top-8 right-8 z-10 rounded-full">
           Buy Now
         </Badge>
-        <div className="relative">
+        <div className="relative w-full">
           <Lens hovering={hovering} setHovering={setHovering} lensSize={100}>
             <Image
               src={productImage}
               alt="image"
-              width={250}
-              height={250}
-              className="aspect-square object-contain w-1/2 mx-auto rounded-2xl"
+              width={1080}
+              height={1080}
+              className="object-cover aspect-video"
             />
           </Lens>
           <motion.div
             animate={{
-              filter: hovering ? 'blur(2px)' : 'blur(0px)',
+              filter: hovering ? "blur(2px)" : "blur(0px)",
             }}
             className="py-4 px-6 rounded-sm relative mt-4 bg-white"
           >

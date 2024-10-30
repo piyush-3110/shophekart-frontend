1a1970ec2fd7c72cd0013d1461fe490508bfe831
Umar <khanumar0108@gmail.com>
2024-10-30 14:52:42 +0530

    style: Fix product details formatting on single product page

--- src/components/ItemDetails/ItemDescriptionText.tsx
+++ src/components/ItemDetails/ItemDescriptionText.tsx
@@ -5,13 +5,16 @@ interface ItemDescriptionTextProps {
   details: string;
 }
 
-const ItemDescriptionText: React.FC<ItemDescriptionTextProps> = ({ description, details }) => {
+const ItemDescriptionText: React.FC<ItemDescriptionTextProps> = ({
+  description,
+  details,
+}) => {
   return (
     <div>
       <p className="text-sm font-[400] text-[#6B6F93] mb-2">{description}</p>
       {/* Render the 'details' as HTML */}
       <div
-        className="text-sm font-[400] text-[#6B6F93]"
+        className="prose prose-h1:text-lg"
         dangerouslySetInnerHTML={{ __html: details }}
       />
     </div>

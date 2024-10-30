15b86b0e176d1223e0a19b63bab0a1d4cef4413f
Umar <khanumar0108@gmail.com>
2024-10-31 00:45:49 +0530

    feat: Redirect to cshopCard page from homepage partners section

--- src/components/Homepage/Partners/Partners.tsx
+++ src/components/Homepage/Partners/Partners.tsx
@@ -1,115 +1,117 @@
-import React from 'react';
-import Image from 'next/image';
-import MarqueeComp from './MarqueeComp';
+import React from "react";
+import Image from "next/image";
+import MarqueeComp from "./MarqueeComp";
+import Link from "next/link";
 // import { PurchaseHistory } from './PurchaseHistory';
 
 const Partners = () => {
   return (
-    <div className='relative overflow-x-hidden min-h-[100vh] overflow-hidden pb-8 pt-12 flex gap-12 flex-col items-center bg-white'>
-      <h1 className='text-black font-semibold md:text-3xl text-lg text-center'>
+    <div className="relative overflow-x-hidden min-h-[100vh] overflow-hidden pb-8 pt-12 flex gap-12 flex-col items-center bg-white">
+      <h1 className="text-black font-semibold md:text-3xl text-lg text-center">
         Partners and Supporters
       </h1>
       <div>
         <MarqueeComp />
       </div>
-      <div className='flex mt-4 justify-center gap-16'>
-        <div className='flex flex-col items-center md:items-start md:pl-12 gap-6 mt-6 w-[90vw] md:w-[90vw] lg:w-[40vw]'>
-          <h1 className='text-black font-semibold md:text-3xl text-lg'>
+      <div className="flex mt-4 justify-center gap-16">
+        <div className="flex flex-col items-center md:items-start md:pl-12 gap-6 mt-6 w-[90vw] md:w-[90vw] lg:w-[40vw]">
+          <h1 className="text-black font-semibold md:text-3xl text-lg">
             Introducing the Crypto Shophekart Debit Card
           </h1>
-          <p className='text-[#6B6F93] font-[400] text-[16px]'>
+          <p className="text-[#6B6F93] font-[400] text-[16px]">
             Seamlessly Convert Your Crypto into Cash Anywhere.
           </p>
 
-          <div className='flex gap-8'>
-            <div className='flex flex-col text-[#6B6F93] font-[400] text-[16px]'>
-              <div className='flex lg:items-center'>
+          <div className="flex gap-8">
+            <div className="flex flex-col text-[#6B6F93] font-[400] text-[16px]">
+              <div className="flex lg:items-center">
                 <Image
-                  src='/images/homepage/Partners/ellipse.png'
-                  alt='Logo'
+                  src="/images/homepage/Partners/ellipse.png"
+                  alt="Logo"
                   width={16}
                   height={16}
-                  className='h-3 w-3 mr-2'
+                  className="h-3 w-3 mr-2"
                 />
                 <p>Multi-Currency support</p>
               </div>
-              <div className='flex lg:items-center'>
+              <div className="flex lg:items-center">
                 <Image
-                  src='/images/homepage/Partners/ellipse.png'
-                  alt='Logo'
+                  src="/images/homepage/Partners/ellipse.png"
+                  alt="Logo"
                   width={16}
                   height={16}
-                  className='h-3 w-3 mr-2'
+                  className="h-3 w-3 mr-2"
                 />
                 <p>CSHOP Token Integration</p>
               </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <Image
-                  src='/images/homepage/Partners/ellipse.png'
-                  alt='Logo'
+                  src="/images/homepage/Partners/ellipse.png"
+                  alt="Logo"
                   width={16}
                   height={16}
-                  className='h-3 w-3 mr-2'
+                  className="h-3 w-3 mr-2"
                 />
                 <p>Look Exchange Rates</p>
               </div>
             </div>
-            <div className='flex flex-col text-[#6B6F93] font-[400] text-[16px]'>
-              <div className='flex items-center'>
+            <div className="flex flex-col text-[#6B6F93] font-[400] text-[16px]">
+              <div className="flex items-center">
                 <Image
-                  src='/images/homepage/Partners/ellipse.png'
-                  alt='Logo'
+                  src="/images/homepage/Partners/ellipse.png"
+                  alt="Logo"
                   width={16}
                   height={16}
-                  className='h-3 w-3 mr-2'
+                  className="h-3 w-3 mr-2"
                 />
                 <p>Virtual and Physical Card</p>
               </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <Image
-                  src='/images/homepage/Partners/ellipse.png'
-                  alt='Logo'
+                  src="/images/homepage/Partners/ellipse.png"
+                  alt="Logo"
                   width={16}
                   height={16}
-                  className='h-3 w-3 mr-2'
+                  className="h-3 w-3 mr-2"
                 />
                 <p>Crypto Currency Security</p>
               </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <Image
-                  src='/images/homepage/Partners/ellipse.png'
-                  alt='Logo'
+                  src="/images/homepage/Partners/ellipse.png"
+                  alt="Logo"
                   width={16}
                   height={16}
-                  className='h-3 w-3 mr-2'
+                  className="h-3 w-3 mr-2"
                 />
                 <p>And much more</p>
               </div>
             </div>
           </div>
-          <button className='gradient-button w-[10rem]'>Learn More</button>
+          <Link href={"/cshopCard"} className="gradient-button w-[10rem]">
+            Learn More
+          </Link>
         </div>
 
-        <div className='lg:block hidden z-[1] relative mt-8'>
+        <div className="lg:block hidden z-[1] relative mt-8">
           <Image
-            alt='MasterCard'
-            src='/images/homepage/Partners/mastercard1.png'
+            alt="MasterCard"
+            src="/images/homepage/Partners/mastercard1.png"
             height={524}
             width={524}
-            className='h-[40vh] w-auto rotate-left'
+            className="h-[40vh] w-auto rotate-left"
           />
           <Image
-            alt='MasterCard'
-            src='/images/homepage/Partners/mastercard2.png'
+            alt="MasterCard"
+            src="/images/homepage/Partners/mastercard2.png"
             height={524}
             width={524}
-            className='h-[50vh] w-auto absolute z-[1] top-0 -right-10 rotate-right'
+            className="h-[50vh] w-auto absolute z-[1] top-0 -right-10 rotate-right"
           />
         </div>
       </div>
-    {/* <PurchaseHistory/> */}
+      {/* <PurchaseHistory/> */}
       <div className="blob" />
-
     </div>
   );
 };

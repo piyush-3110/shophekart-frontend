c7170e88803b021bc25c67fe85326e83008a4137
Sakshat kumar <sakshatkumar007@gmail.com>
2024-10-30 14:32:10 +0530

    fix: fix searched bar

--- src/components/products/shared/Searchbar.tsx
+++ src/components/products/shared/Searchbar.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { useUserStore } from "@/store"; // Import Zustand store
+import { useUserStore } from "@/store";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 const Searchbar = () => {
-  const setSearchTerm = useUserStore((state) => state.setSearchTerm); // Set search term in Zustand store
-  const [localSearch, setLocalSearch] = useState(""); // Local state for input value
+  const setSearchTerm = useUserStore((state) => state.setSearchTerm);
+  const [localSearch, setLocalSearch] = useState<string>("");
 
-  // Handle search button click
   const handleSearch = () => {
-    if (localSearch.trim()) { // Ensure there's a search term before proceeding
-      setSearchTerm(localSearch); // Set the search term only when the search is triggered
+    if (localSearch.trim()) {
+      setSearchTerm(localSearch);
+    }
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
   };
 
@@ -19,7 +24,8 @@ const Searchbar = () => {
     <div className="flex items-center relative h-12 w-full max-w-lg md:max-w-4xl">
       <Input
         value={localSearch}
-        onChange={(e) => setLocalSearch(e.target.value)} // Update local state with input value
+        onChange={(e) => setLocalSearch(e.target.value)}
+        onKeyDown={handleKeyDown} 
         placeholder="Search products"
         className="py-4 pl-12 rounded-r-none rounded-l size-full peer"
       />
@@ -28,13 +34,13 @@ const Searchbar = () => {
         height={18}
         src={"/icons/productNavbar/searchIcon.svg"}
         alt="search icon"
-        className="absolute top-1/2 -translate-y-1/2 left-4 cursor-pointer" // Added cursor pointer
-        onClick={handleSearch} // Trigger search on clicking the search icon
+        className="absolute top-1/2 -translate-y-1/2 left-4 cursor-pointer"
+        onClick={handleSearch}
       />
-      {/* Remove the button for small screens */}
+
       <button
-        onClick={handleSearch} // Trigger search when the button is clicked (for larger screens)
-        className="hidden gradient-button text-white md:inline" // Only show on medium screens and up
+        onClick={handleSearch}
+        className="hidden gradient-button text-white md:inline"
       >
         Search
       </button>

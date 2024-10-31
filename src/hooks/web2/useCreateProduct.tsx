a0dff32b8b3ae37fb3d956e14ba46f2ab42f389c
Umar <khanumar0108@gmail.com>
2024-10-31 20:18:19 +0530

    fix: Delete product from database if error from blockchain

--- src/hooks/web2/useCreateProduct.tsx
+++ src/hooks/web2/useCreateProduct.tsx
@@ -10,65 +10,73 @@ import { parseEther } from "viem";
 import { PRODUCT_CREATION_FEE } from "@/constants/application";
 
 const currencyTypeAddresses = {
-  USDT: TOKEN_ADDRESS.USDT,
-  BNB: TOKEN_ADDRESS.BNB,
-  CSHOP: TOKEN_ADDRESS.CSHOP,
-  USDC: TOKEN_ADDRESS.USDC,
+    USDT: TOKEN_ADDRESS.USDT,
+    BNB: TOKEN_ADDRESS.BNB,
+    CSHOP: TOKEN_ADDRESS.CSHOP,
+    USDC: TOKEN_ADDRESS.USDC,
 };
 
 export default function useCreateProduct(userWalletAddress: `0x${string}`) {
-  const { writeContractAsync, setProductId } =
-    useCreateProductOnChain(userWalletAddress);
+    const { writeContractAsync, setProductId } =
+        useCreateProductOnChain(userWalletAddress);
 
-  const { mutateAsync } = useMutation({
-    async mutationFn(data: FormData) {
-      const response = await HttpRequestService.postApi<IProduct, FormData>(
-        "/fixedProduct/create",
-        data,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+    const { mutateAsync } = useMutation({
+        async mutationFn(data: FormData) {
+            const response = await HttpRequestService.postApi<IProduct, FormData>(
+                "/fixedProduct/create",
+                data,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
 
-      return response;
-    },
-    async onSuccess({ data: product }) {
-      const tokenUri = await createTokenUri({
-        name: product.name,
-        category: product.category,
-        currencyType: product.currencyType,
-        description: product.description,
-        image: product.images[0],
-        price: product.price.toString(),
-      });
+            return response;
+        },
+        async onSuccess({ data: product }) {
+            const tokenUri = await createTokenUri({
+                name: product.name,
+                category: product.category,
+                currencyType: product.currencyType,
+                description: product.description,
+                image: product.images[0],
+                price: product.price.toString(),
+            });
 
-      await writeContractAsync({
-        ...CONTRACT_CONFIG.marketplace,
-        functionName: "createMarketItem",
-        args: [
-          product.stock,
-          currencyTypeAddresses[product.currencyType],
-          parseEther(product.price.toString()),
-          parseEther(product.shippingCharges.toString()),
-          tokenUri,
-        ],
-        value: parseEther(PRODUCT_CREATION_FEE),
-      });
+            try {
+                await writeContractAsync({
+                    ...CONTRACT_CONFIG.marketplace,
+                    functionName: "createMarketItem",
+                    args: [
+                        product.stock,
+                        currencyTypeAddresses[product.currencyType],
+                        parseEther(product.price.toString()),
+                        parseEther(product.shippingCharges.toString()),
+                        tokenUri,
+                    ],
+                    value: parseEther(PRODUCT_CREATION_FEE),
+                });
+            } catch {
+                if (product._id) {
+                    await HttpRequestService.deleteApi<IProduct>(
+                        `/product/${product._id}/delete`
+                    );
+                }
+            }
 
-      setProductId(product._id);
-    },
-    onError(error) {
-      console.log(error);
-      toast({
-        title: "Error creating product",
-        description:
-          "There was an error while creating the product. Please try again later.",
-        variant: "destructive",
-      });
-    },
-  });
+            setProductId(product._id);
+        },
+        onError(error) {
+            console.log(error);
+            toast({
+                title: "Error creating product",
+                description:
+                    "There was an error while creating the product. Please try again later.",
+                variant: "destructive",
+            });
+        },
+    });
 
-  return { mutateAsync };
+    return { mutateAsync };
 }

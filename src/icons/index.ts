6a0d4b0984ba5d1b7f0af2485d8256115b4110c1
Umar <khanumar0108@gmail.com>
2024-11-01 16:58:46 +0530

    feat: Add Like icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -8,3 +8,4 @@ export { default as HistoryIcon } from "./HistoryIcon";
 export { default as LogoutIcon } from "./LogoutIcon";
 export { default as UserProfileIcon } from "./UserProfileIcon";
 export { default as AiShopheLogo } from "./AiShopheLogo";
+export { default as LikeIcon } from "./LikeIcon";

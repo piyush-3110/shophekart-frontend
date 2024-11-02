424ef792ec3f3dc8fecb5b46595dcca07783f354
Umar <khanumar0108@gmail.com>
2024-11-02 23:51:49 +0530

    feat: Add post order functions for buyer and seller

--- src/components/orderDetail/SingleDetail.tsx
+++ src/components/orderDetail/SingleDetail.tsx
@@ -5,9 +5,15 @@ import ItemDescription from './ItemDescription';
 import BuyerDetails from './BuyerDetails';
 import { useQuery } from '@tanstack/react-query';
 import ItemCard from '../ItemDetails/ItemCard';
+import FetchError from '../shared/FetchError';
+import UpdateOrderStatus from './UpdateOrderStatus';
+import CancelOrder from './CancelOrder';
+import ClaimFunds from './ClaimFunds';
+import ConfirmDelivery from './ConfirmDelivery';
+import RaiseDispute from './RaiseDispute';
 
 const OrderDetail: React.FC<ItemDetailProps> = ({ orderId }) => {
-    const { data: order, isLoading, error } = useQuery({
+    const { data: order, isLoading, error, refetch } = useQuery({
         queryKey: ['order', orderId],
         queryFn: async () => {
             const { data } = await httpRequestService.fetchApi<TOrder>(`/order/single/${orderId}`);
@@ -18,15 +24,14 @@ const OrderDetail: React.FC<ItemDetailProps> = ({ orderId }) => {
     if (isLoading) return <div>Loading...</div>;
 
     if (error || !order) {
-        return <div>Error fetching data</div>
+        return <FetchError refetch={refetch} />
     }
-
     return (
         <div className="max-w-screen-xl mx-auto p-4">
             <div className="flex flex-col lg:flex-row gap-6 items-start justify-center">
                 <ItemCard images={order.product.images} />
                 <ItemDescription
-                    nftId={order.product.nftId}
+                    nftId={order.nftId}
                     title={order.product.name}
                     description={order.product.description}
                     price={order.soldAtPrice}
@@ -38,6 +43,7 @@ const OrderDetail: React.FC<ItemDetailProps> = ({ orderId }) => {
                     status={order.orderStatus}
                 />
             </div>
+            {order.isCallerBuyer ? <BuyerActions orderId={order._id} currentOrderStatus={order.orderStatus} /> : order.isCallerSeller ? <SellerActions deliveryBy={order.deliveryBy} nftId={order.nftId} orderId={order._id} currentOrderStatus={order.orderStatus} /> : null}
             <BuyerDetails
                 firstName={order.shippingAddress.firstName}
                 lastName={order.shippingAddress.lastName}
@@ -54,6 +60,47 @@ const OrderDetail: React.FC<ItemDetailProps> = ({ orderId }) => {
 };
 
 export default OrderDetail;
+
+function BuyerActions({ orderId, currentOrderStatus }: { orderId: string, currentOrderStatus: OrderStatus }) {
+    return <div className="p-6 flex flex-wrap gap-10 items-center">
+        <div className='space-y-4'>
+            <h2>
+                Confirm Delivery
+            </h2>
+            <ConfirmDelivery orderId={orderId} currentStatus={currentOrderStatus} />
+        </div>
+        <div className='space-y-4'>
+            <h2>
+                Cancel Order
+            </h2>
+            <CancelOrder orderId={orderId} currentOrderStatus={currentOrderStatus} />
+        </div>
+        <div className='space-y-4'>
+            <h2>
+                Raise dispute
+            </h2>
+            <RaiseDispute orderId={orderId} currentOrderStatus={currentOrderStatus}/>
+        </div>
+    </div>
+}
+
+
+function SellerActions({ orderId, currentOrderStatus, nftId , deliveryBy}: { orderId: string, currentOrderStatus: OrderStatus, nftId: number, deliveryBy:Date }) {
+    return <div className="p-6 flex flex-wrap gap-10 items-start">
+       {currentOrderStatus==="pending"&& <div className='space-y-4'>
+            <h2>
+                Update the delivery status
+            </h2>
+            <UpdateOrderStatus orderId={orderId} currentOrderStatus={currentOrderStatus} />
+        </div>}
+        <div className='space-y-4'>
+        <h2>
+                Claim funds
+            </h2>
+            <ClaimFunds deliveryBy={deliveryBy} currentOrderStatus={currentOrderStatus} nftId={nftId} />
+        </div>
+    </div>
+}
 
 type Address = {
     state: string
@@ -77,7 +124,6 @@ type Product = {
     type: "FixedProduct" | "Auction"
     category: string
     name: string
-    nftId: number
 }
 
 export type OrderStatus = "pending" | "delivering" | "delivered" | "cancelled" | "dispute";
@@ -92,6 +138,9 @@ type TOrder = {
     shippingAddress: Address
     shippingPrice: number
     buyer: Buyer
+    isCallerBuyer: boolean
+    isCallerSeller: boolean
+    nftId: number
 }
 
 interface ItemDetailProps {

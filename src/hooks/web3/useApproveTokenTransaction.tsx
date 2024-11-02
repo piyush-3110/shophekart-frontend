f3ca2bfec160bc2a161c9eff3de51005f03b191b
Umar <khanumar0108@gmail.com>
2024-11-02 23:50:06 +0530

    feat: Add custom hook to approve transaction for tokens

---
+++ src/hooks/web3/useApproveTokenTransaction.tsx
@@ -0,0 +1,25 @@
+import { useWriteContract } from "wagmi";
+import { config } from "@/config"
+import CONTRACT_CONFIG from "@/constants/contractConfig";
+import { parseEther } from "viem";
+import { waitForTransactionReceipt } from "@wagmi/core";
+
+export default function useApproveTokenTransaction() {
+
+    const { writeContractAsync , ...props} = useWriteContract({ config })
+
+    async function approveTokenTransaction(token: string, amount:number) {
+     const hash=   await writeContractAsync({
+            ...CONTRACT_CONFIG[token],
+            functionName: "approve",
+            args: [
+              CONTRACT_CONFIG.marketplace.address,
+              parseEther((amount).toString()),
+            ],
+        })
+
+        await waitForTransactionReceipt(config,{ hash })
+
+    }
+    return { approveTokenTransaction , ...props}
+}

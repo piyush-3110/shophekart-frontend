11e6204b8e60f40f2d2c3600b89b66163435d267
Umar <khanumar0108@gmail.com>
2024-11-03 21:35:04 +0530

    feat: Add type for shipping address in item description component

--- src/components/ItemDetails/ItemDescription.tsx
+++ src/components/ItemDetails/ItemDescription.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { useEffect, useState } from "react";
 import ItemInfoHeader from "./ItemInfoHeader";
@@ -10,124 +9,151 @@ import useCreateOrder from "@/hooks/web2/useCreateOrder";
 import { Button } from "../ui/button";
 import { UpdateShippingModal } from "./UpdateShippingModal";
 import ShippingModal from "./ShippingModal";
-
 import httpRequestService from "@/services/httpRequest.service";
 
-interface ItemDescriptionProps {
-  name: string;
-  description: string;
-  details: string;
-  price: number;
-  currencyType: TCurrencyType;
-  buyerId: string;
-  shippingDuration: number;
-  stock: number;
-  id: string;
-  shippingType: string;
-  productIdOnChain: number;
-  shippingCharges: number;
-  stars: number;
-  reviewCount: number;
-  address:string;
-}
-
 export const ItemDescription: React.FC<ItemDescriptionProps> = ({
-    ...params
+	...params
 }) => {
-    const { user, authStatus } = useUserStore();
+	const { user, authStatus } = useUserStore();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [hasShippingAddress, setHasShippingAddress] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [hasShippingAddress, setHasShippingAddress] = useState(false);
 
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
-    };
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-    useEffect(() => {
-        const fetchShippingAddress = async () => {
-          try {
-            const response = await httpRequestService.fetchApi<any>("/shipping-address/me");
-            if (response?.data.length!=0) {
-              setHasShippingAddress(true);
-            }
-          } catch (error) {
-            console.error("Error fetching shipping address:", error);
-            setHasShippingAddress(false);
-          }
-        };
+	useEffect(() => {
+		const fetchShippingAddress = async () => {
+			try {
+				const response = await httpRequestService.fetchApi<TShippingAddress[]>(
+					"/shipping-address/me"
+				);
+				if (response?.data.length != 0) {
+					setHasShippingAddress(true);
+				}
+			} catch (error) {
+				console.error("Error fetching shipping address:", error);
+				setHasShippingAddress(false);
+			}
+		};
 
-        fetchShippingAddress();},[user])
+		fetchShippingAddress();
+	}, [user]);
 
-    const { createOrder, isLoading } = useCreateOrder(user?.walletAddress ?? "0x0000000000000000000000000000000000000000")
+	const { createOrder, isLoading } = useCreateOrder(
+		user?.walletAddress ?? "0x0000000000000000000000000000000000000000"
+	);
 
-    async function handleOrderCreate(shippingAddressId: string) {
-        await createOrder({ shippingAddressId, buyerId: params.buyerId, productId: params.id, productIdOnChain: params.productIdOnChain, shippingDuration: params.shippingDuration, shippingPrice: params.shippingCharges }, params.price, params.currencyType)
-    }
+	async function handleOrderCreate(shippingAddressId: string) {
+		await createOrder(
+			{
+				shippingAddressId,
+				buyerId: params.buyerId,
+				productId: params.id,
+				productIdOnChain: params.productIdOnChain,
+				shippingDuration: params.shippingDuration,
+				shippingPrice: params.shippingCharges,
+			},
+			params.price,
+			params.currencyType
+		);
+	}
 
-    return (
-        <div className="flex flex-col gap-3 pl-4 md:pl-0 w-[95vw] md:w-[50vw] lg:w-[40vw]">
-            <ItemInfoHeader
-                name={params.name}
-                stock={params.stock}
-                reviewCount={params.reviewCount}
-                stars={params.stars}
-            />
-            <ItemDescriptionText description={params.description} details={params.details} />
+	return (
+		<div className="flex flex-col gap-3 pl-4 md:pl-0 w-[95vw] md:w-[50vw] lg:w-[40vw]">
+			<ItemInfoHeader
+				name={params.name}
+				stock={params.stock}
+				reviewCount={params.reviewCount}
+				stars={params.stars}
+			/>
+			<ItemDescriptionText
+				description={params.description}
+				details={params.details}
+			/>
 
-      <div className="flex flex-col md:flex-row md:gap-6 md:items-center">
-        <h1 className="text-[#160041] font-[700] text-xl">
-          {params.price} {params.currencyType}
-        </h1>
-        <div className="flex gap-2 items-center">
-          <p className="text-sm font-[400] text-[#6B6F93]">
-            {params.shippingType === "LOCAL" ? `Local Shipping (${params.address})` : "Global Shipping"}
-          </p>
-        </div>
-      </div>
+			<div className="flex flex-col md:flex-row md:gap-6 md:items-center">
+				<h1 className="text-[#160041] font-[700] text-xl">
+					{params.price} {params.currencyType}
+				</h1>
+				<div className="flex gap-2 items-center">
+					<p className="text-sm font-[400] text-[#6B6F93]">
+						{params.shippingType === "LOCAL"
+							? `Local Shipping (${params.address})`
+							: "Global Shipping"}
+					</p>
+				</div>
+			</div>
 
-            {authStatus === "authenticated" ? (
-                <Button
-                    className="gradient-button mt-3 !text-sm !w-fit"
-                    onClick={handleOpenModal}
-                    disabled={isLoading||params.stock==0}
-                >
-                   {params.stock === 0 ? "Out of Stock" : isLoading ? "Buying..." : "Buy Now"}
-                </Button>
-            ) : (
-                <ConnectWalletButton />
-            )}
-            {isModalOpen && (
-        hasShippingAddress ? (
-                  <UpdateShippingModal
-                      isOpen={isModalOpen}
-                      onClose={handleCloseModal}
-                      onOrderCreate={handleOrderCreate}
-                  />
-        ) : (
-          <ShippingModal isOpen={isModalOpen} onClose={handleCloseModal} onClose1={handleCloseModal} onOrderCreate={handleOrderCreate}/>
-        )
-            )}
-        </div>
-    );
+			{authStatus === "authenticated" ? (
+				<Button
+					className="gradient-button mt-3 !text-sm !w-fit"
+					onClick={handleOpenModal}
+					disabled={isLoading || params.stock == 0}
+				>
+					{params.stock === 0
+						? "Out of Stock"
+						: isLoading
+						? "Buying..."
+						: "Buy Now"}
+				</Button>
+			) : (
+				<ConnectWalletButton />
+			)}
+			{isModalOpen &&
+				(hasShippingAddress ? (
+					<UpdateShippingModal
+						isOpen={isModalOpen}
+						onClose={handleCloseModal}
+						onOrderCreate={handleOrderCreate}
+					/>
+				) : (
+					<ShippingModal
+						isOpen={isModalOpen}
+						onClose={handleCloseModal}
+						onClose1={handleCloseModal}
+						onOrderCreate={handleOrderCreate}
+					/>
+				))}
+		</div>
+	);
 };
 
 interface ItemDescriptionProps {
-    name: string;
-    description: string;
-    details: string;
-    price: number;
-    currencyType: TCurrencyType;
-    buyerId: string;
-    shippingDuration: number;
-    stock: number;
-    id: string;
-    shippingType: string;
-    productIdOnChain: number;
-    shippingCharges: number;
-    stars: number;
-    reviewCount: number;
+	name: string;
+	description: string;
+	details: string;
+	price: number;
+	currencyType: TCurrencyType;
+	buyerId: string;
+	shippingDuration: number;
+	stock: number;
+	id: string;
+	shippingType: string;
+	productIdOnChain: number;
+	shippingCharges: number;
+	stars: number;
+	reviewCount: number;
+	address: string;
 }
+
+type TShippingAddress = {
+	_id: string;
+	address: string;
+	city: string;
+	state: string;
+	postalCode: string;
+	country: string;
+	buyerId: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	phoneNumber: string;
+	createdAt: Date;
+	updatedAt: Date;
+};

9638b5e7b60d8781e06281f0e0d2baf64c8c0621
Umar <khanumar0108@gmail.com>
2024-11-03 20:57:51 +0530

    feat: Change add product page link

--- src/components/Navbar/RenderedNavbar.tsx
+++ src/components/Navbar/RenderedNavbar.tsx
@@ -7,30 +7,33 @@ import Searchbar from "../products/shared/Searchbar";
 import Link from "next/link";
 
 const RenderedNavbar = () => {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  // if (pathname == "/products/buy-now") {
-  //   return <Navbar className="mb-10" />;
-  // }
-  // return <FloatingNavbar />;
-  if (pathname == "/products/buy-now") {
-    return (
-      <>
-        <FloatingNavbar />
-        <div className="flex w-full items-center justify-between px-4 lg:px-32 mt-4 mb-8">
-          <div className="w-[45vw] lg:w-[600px]">
-            <Searchbar />
-          </div>
-          <Link href="/form" className="">
-            <button className="text-white gradient-button w-auto lg:w-auto">
-              Add Product
-            </button>
-          </Link>
-        </div>
-      </>
-    );
-  }
-  return <FloatingNavbar />;
+	// if (pathname == "/products/buy-now") {
+	//   return <Navbar className="mb-10" />;
+	// }
+	// return <FloatingNavbar />;
+	if (pathname == "/products/buy-now") {
+		return (
+			<>
+				<FloatingNavbar />
+				<div className="flex w-full items-center justify-between px-4 lg:px-32 mt-4 mb-8">
+					<div className="w-[45vw] lg:w-[600px]">
+						<Searchbar />
+					</div>
+					<Link
+						href="/add-product"
+						className=""
+					>
+						<button className="text-white gradient-button w-auto lg:w-auto">
+							Add Product
+						</button>
+					</Link>
+				</div>
+			</>
+		);
+	}
+	return <FloatingNavbar />;
 };
 
 export default RenderedNavbar;

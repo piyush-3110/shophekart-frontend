df35f0f0571645e00bd3e8d7d0bd683b8fab4395
Umar <khanumar0108@gmail.com>
2024-11-04 03:18:32 +0530

    feat: Add cshop token sale contract config

--- src/constants/ABI/index.ts
+++ src/constants/ABI/index.ts
@@ -1,3 +1,4 @@
 export { default as MARKETPLACE_ABI } from "./MARKETPLACE_ABI.json";
 export { default as ESCROW_ABI } from "./ESCROW_ABI.json";
 export { default as ERC20_ABI } from "./ERC20.json";
+export { default as CSHOP_TOKEN_SALE_ABI } from "./CSHOP_TOKEN_SALE.json";
--- src/constants/contractConfig/index.ts
+++ src/constants/contractConfig/index.ts
@@ -1,30 +1,39 @@
-import { ERC20_ABI, ESCROW_ABI, MARKETPLACE_ABI } from "../ABI";
+import {
+	CSHOP_TOKEN_SALE_ABI,
+	ERC20_ABI,
+	ESCROW_ABI,
+	MARKETPLACE_ABI,
+} from "../ABI";
 import TOKEN_ADDRESS from "../tokenAddress";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type TContractConfig = Record<string, { address: `0x${string}`; abi: any }>;
 
 const CONTRACT_CONFIG: TContractConfig = {
-  marketplace: {
-    address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
-    abi: MARKETPLACE_ABI,
-  },
-  escrow: {
-    address: "0x5540e5cf498B4D7278ad6D6C58E7800f2450204c",
-    abi: ESCROW_ABI,
-  },
-  usdt: {
-    address: TOKEN_ADDRESS.USDT,
-    abi: ERC20_ABI,
-  },
-  usdc: {
-    address: TOKEN_ADDRESS.USDC,
-    abi: ERC20_ABI,
-  },
-  cshop: {
-    address: TOKEN_ADDRESS.CSHOP,
-    abi: ERC20_ABI,
-  },
+	marketplace: {
+		address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
+		abi: MARKETPLACE_ABI,
+	},
+	escrow: {
+		address: "0x5540e5cf498B4D7278ad6D6C58E7800f2450204c",
+		abi: ESCROW_ABI,
+	},
+	usdt: {
+		address: TOKEN_ADDRESS.USDT,
+		abi: ERC20_ABI,
+	},
+	usdc: {
+		address: TOKEN_ADDRESS.USDC,
+		abi: ERC20_ABI,
+	},
+	cshop: {
+		address: TOKEN_ADDRESS.CSHOP,
+		abi: ERC20_ABI,
+	},
+	cshopTokenSale: {
+		address: "0xE11CC106ba82D28Ff07733d6cfb65AEbaf3F6C5d",
+		abi: CSHOP_TOKEN_SALE_ABI,
+	},
 };
 
 export default CONTRACT_CONFIG;

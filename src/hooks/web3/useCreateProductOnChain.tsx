ce8c149de7f803c324d08e0847229d3f3a71d896
Umar <khanumar0108@gmail.com>
2024-11-03 21:04:25 +0530

    feat: Add custom hook to create product on chain

--- src/hooks/web3/useCreateProductOnChain.tsx
+++ src/hooks/web3/useCreateProductOnChain.tsx
@@ -1,94 +1,64 @@
 import { config } from "@/config";
 import CONTRACT_CONFIG from "@/constants/contractConfig";
-import { HttpRequestService } from "@/services";
-import { IProduct } from "@/types";
-import { useMutation } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 import {
-  useReadContract,
-  useWaitForTransactionReceipt,
-  useWriteContract,
+	useReadContract,
+	useWaitForTransactionReceipt,
+	useWriteContract,
 } from "wagmi";
-import { toast } from "../use-toast";
+import { PRODUCT_CREATION_FEE } from "@/constants/application";
+import { parseEther } from "viem";
 
 const useCreateProductOnChain = (userWalletAddress: `0x${string}`) => {
-  const [productId, setProductId] = useState<string | null>(null);
+	const {
+		writeContractAsync,
+		data: hash,
+		...props
+	} = useWriteContract({
+		config,
+	});
 
-  const { writeContractAsync, data: hash } = useWriteContract({
-    config,
-    mutation: {
-      async onError() {
-        toast({
-          title: "Error creating product",
-          description:
-            "There was an error while creating the product. Please try again later.",
-          variant: "destructive",
-        });
-        if (productId) {
-          await HttpRequestService.deleteApi<IProduct>(
-            `/product/${productId}/delete`
-          );
-        }
-      },
-    },
-  });
+	async function createProductOnChain({
+		stock,
+		currencyAddress,
+		price,
+		shippingCharges,
+		tokenUri,
+	}: TCreateProductOnChain) {
+		await writeContractAsync({
+			...CONTRACT_CONFIG.marketplace,
+			functionName: "createMarketItem",
+			args: [
+				stock,
+				currencyAddress,
+				parseEther(price.toString()),
+				parseEther(shippingCharges.toString()),
+				tokenUri,
+			],
+			value: parseEther(PRODUCT_CREATION_FEE),
+		});
+	}
 
-  const { isSuccess } = useWaitForTransactionReceipt({ hash, config });
+	const { isSuccess } = useWaitForTransactionReceipt({ hash, config });
 
-  const { data: onChainIds } = useReadContract({
-    config,
-    ...CONTRACT_CONFIG.marketplace,
-    functionName: "getUserListedItemIds",
-    args: [userWalletAddress],
-    query: {
-      enabled: isSuccess,
-    },
-  });
+	const { data: onChainIds } = useReadContract({
+		config,
+		...CONTRACT_CONFIG.marketplace,
+		functionName: "getUserListedItemIds",
+		args: [userWalletAddress],
+		query: {
+			enabled: isSuccess,
+		},
+	});
 
-  const { mutateAsync } = useMutation({
-    async mutationFn(onChainId: string) {
-      if (!productId) {
-        throw new Error("Something went wrong while creating product");
-      }
-      console.log(productId);
-      const response = await HttpRequestService.updateApi<
-        IProduct,
-        { onChainId: number; productId: string }
-      >(`/product/${productId}/update-on-chain-id`, {
-        onChainId: Number(onChainId),
-        productId,
-      });
-
-      return response.data;
-    },
-    onSuccess() {
-      toast({
-        title: "Product created successfully",
-      });
-    },
-    onError() {
-      toast({
-        title: "Error creating product",
-        description:
-          "There was an error while creating the product. Please try again later.",
-        variant: "destructive",
-      });
-    },
-  });
-
-  useEffect(() => {
-    if (isSuccess && onChainIds) {
-      (async () => {
-        const productId = (onChainIds as bigint[])[
-          (onChainIds as number[]).length - 1
-        ];
-        console.log(productId.toString());
-        await mutateAsync(productId.toString());
-      })();
-    }
-  }, [isSuccess, onChainIds, mutateAsync, hash]);
-
-  return { writeContractAsync, setProductId };
+	return { createProductOnChain, ...props, onChainIds, isSuccess };
 };
 
 export default useCreateProductOnChain;
+
+type TCreateProductOnChain = {
+	stock: number;
+	currencyAddress: `0x${string}`;
+	price: number;
+	shippingCharges: number;
+	tokenUri: string; // nft metadata
+};

ecf78e142d3f25223c432d6d7cc6ecdb522ab6fc
Umar <khanumar0108@gmail.com>
2024-11-04 14:24:50 +0530

    feat: Add functionality to buy cshop token

--- src/components/Homepage/Presale/BuyToken.tsx
+++ src/components/Homepage/Presale/BuyToken.tsx
@@ -1,77 +1,142 @@
-import React, { useState } from 'react';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
-import ConnectButton from '../Hero/Navbar/ConnectButton';
+import React, { useEffect, useState } from "react";
+import ConnectButton from "../Hero/Navbar/ConnectButton";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import useBuyCshopTokenForm from "@/hooks/web2/useBuyCShopTokenForm";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import useGetUserTokenBalance from "@/hooks/web3/useGetUserBalance";
+import { useUserStore } from "@/store";
 
 export const BuyToken: React.FC = () => {
-  const [amount, setAmount] = useState<number | string>('');
-  const [selectedToken, setSelectedToken] = useState<string>('USDT');
-  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+	const { isPending, isSuccess, onSubmit, form } = useBuyCshopTokenForm();
 
-  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAmount(e.target.value);
-  };
+	const { user } = useUserStore();
 
-  const handleTokenSelect = (token: string) => {
-    setSelectedToken(token);
-    setDropdownOpen(false);
-  };
+	const {
+		bnbBalance,
+		usdtBalance,
+		isLoading,
+		isError,
+		refetchBnb,
+		refetchCshop,
+		refetchUsdt,
+	} = useGetUserTokenBalance(user?.walletAddress);
 
-  const handleMaxAmount = () => {
+	const handleMaxAmount = () => {
+		if (
+			(form.getValues("currencyType") === "BNB" ||
+				form.getValues("currencyType") === "USDT") &&
+			!isLoading &&
+			!isError &&
+			!!bnbBalance &&
+			!!usdtBalance
+		) {
+			form.getValues("currencyType") === "BNB" &&
+				form.setValue("amount", bnbBalance);
 
-    const maxAmount = 100; 
-    setAmount(maxAmount);
-  };
+			form.getValues("currencyType") === "USDT" &&
+				form.setValue("amount", usdtBalance);
+		}
+	};
 
-  return (
-    <div className="flex flex-col gap-4 bg-white w-full max-w-md">
-      <div className="flex flex-row gap-4">
-      <div className="flex w-[80%] relative">
-  <input
-    type="number"
-    placeholder="Type an amount"
-    value={amount}
-    onChange={handleAmountChange}
-    className="px-4 py-2 border w-full text-sm border-gray-300 rounded-md text-black placeholder-[#c7bfbf] focus:outline-none focus:ring-1 focus:ring-blue-500"
-  />
-  <button
-    onClick={handleMaxAmount}
-    className="text-[#0246FF] font-400 text-[1rem] !text-sm absolute right-3 top-1/2 transform -translate-y-1/2"
-  >
-    Max
-  </button>
-</div>
+	useEffect(() => {
+		if (isSuccess) {
+			form.reset;
+			refetchBnb();
+			refetchCshop();
+			refetchUsdt();
+		}
+	}, [isSuccess]);
 
-        <div className="relative w-[43%]">
-          <button
-            className="w-full px-4 py-2 border border-gray-300 bg-white rounded-md text-black flex justify-between items-center  focus:outline-none focus:ring-1 focus:ring-blue-500"
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-          >
-            {selectedToken}
-            {dropdownOpen ? (
-              <FaChevronUp className="text-[#b2a7a7]" />
-            ) : (
-              <FaChevronDown className="text-[#b2a7a7]" />
-            )}
-          </button>
-          {dropdownOpen && (
-            <div className="absolute mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10 transition-all duration-200 ease-in-out">
-              <div
-                className="px-4 py-2 text-black cursor-pointer hover:bg-gray-100"
-                onClick={() => handleTokenSelect('USDT')}
-              >
-                USDT
-              </div>
-              <div
-                className="px-4 py-2 text-black cursor-pointer hover:bg-gray-100"
-                onClick={() => handleTokenSelect('BNB')}
-              >
-                BNB
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-      <ConnectButton />
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-4 bg-white w-full max-w-md">
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="space-y-2"
+				>
+					<div className="flex flex-row gap-4">
+						<FormField
+							control={form.control}
+							name="amount"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<div className="flex w-[80%] relative">
+											<Input
+												type="number"
+												placeholder="E.g. 10"
+												className="px-4 py-2 border w-full text-sm border-gray-300 rounded-md text-black placeholder-[#c7bfbf] focus:outline-none focus:ring-1 focus:ring-blue-500"
+												{...field}
+												disabled={isPending}
+											/>
+											<button
+												type="button"
+												onClick={handleMaxAmount}
+												className="text-[#0246FF] font-400 text-[1rem] !text-sm absolute right-3 top-1/2 transform -translate-y-1/2"
+											>
+												Max
+											</button>
+										</div>
+									</FormControl>
+									<FormDescription>
+										Please enter the amount of tokens you want to buy with.
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="currencyType"
+							render={({ field }) => (
+								<FormItem>
+									<Select
+										onValueChange={field.onChange}
+										defaultValue={field.value}
+										disabled={isPending}
+									>
+										<FormControl>
+											<SelectTrigger className="w-min">
+												<SelectValue placeholder="Please Select Token" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											<SelectItem value={"BNB"}>{"BNB"}</SelectItem>
+											<SelectItem value={"USDT"}>{"USDT"}</SelectItem>
+										</SelectContent>
+									</Select>
+									<FormDescription>
+										Select the token you want to buy with
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<Button
+						className="gradient-button"
+						type="submit"
+						disabled={isPending}
+					>
+						{isPending ? "Buying..." : "Buy"}
+					</Button>
+				</form>
+			</Form>
+		</div>
+	);
 };

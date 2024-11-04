5a4edebd5f440b6f4e1321c3702c05618c770d8d
Umar <khanumar0108@gmail.com>
2024-11-04 14:24:13 +0530

    feat: Add custom hook for buy token form logic

---
+++ src/hooks/web2/useBuyCShopTokenForm.tsx
@@ -0,0 +1,38 @@
+import buyCshopTokenSchema, {
+	TBuyCShopTokenProps,
+} from "@/validations/buyCshopTokenValidation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import useBuyCshopToken from "../web3/useBuyCshopToken";
+import { useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+export default function useBuyCshopTokenForm() {
+	const form = useForm<TBuyCShopTokenProps>({
+		resolver: zodResolver(buyCshopTokenSchema),
+		defaultValues: {
+			amount: "0",
+			currencyType: "BNB",
+		},
+	});
+
+	const { buyCshopToken, isPending, isSuccess } = useBuyCshopToken();
+
+	const searchParams = useSearchParams();
+
+	const referralCode = searchParams.get("referral");
+
+	const onSubmit = useCallback(
+		async (values: TBuyCShopTokenProps) => {
+			await buyCshopToken({
+				amount: Number(values.amount),
+				token: values.currencyType,
+				referralCode: referralCode ?? undefined,
+			});
+		},
+		[buyCshopToken, referralCode]
+	);
+
+	return { onSubmit, isPending, isSuccess, form };
+}

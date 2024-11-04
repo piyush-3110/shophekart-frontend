6e5d268669f56c0650a0b692418e4850a4cd592c
Umar <khanumar0108@gmail.com>
2024-11-04 23:56:59 +0530

    feat: Add mainnet config to the application

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,25 +1,46 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { bscTestnet } from "wagmi/chains";
+
+// TESTNET
+// import { bscTestnet } from "wagmi/chains";
+
+// MAINNET
+import { bsc } from "wagmi/chains";
 import { http } from "wagmi";
 
 const getOrigin = () => {
-  if (typeof window !== "undefined") {
-    return window.location.origin;
-  }
-  return "https://shophekart-frontend.vercel.app/"; // Fallback for SSR
+	if (typeof window !== "undefined") {
+		return window.location.origin;
+	}
+	return "https://shophekart.com/"; // Fallback for SSR
 };
 
+// TESTNET
+
+// export const config = getDefaultConfig({
+// 	appName: "Shophekart",
+// 	appDescription: "A crypto ecommerce store",
+// 	projectId: "5414a59b644946d2fd01f5710c4784ad",
+// 	chains: [bscTestnet],
+// 	appUrl: getOrigin(),
+// 	appIcon: "/images/shared/logo.png",
+// 	transports: {
+// 		[bscTestnet.id]: http("https://bsc-testnet-rpc.publicnode.com"),
+// 	},
+// 	ssr: true,
+// });
+
+// MAINNET
+
 export const config = getDefaultConfig({
-  appName: "Shophekart",
-  appDescription: "A crypto ecommerce store",
-  projectId: "5414a59b644946d2fd01f5710c4784ad",
-  chains: [bscTestnet],
-  appUrl: getOrigin(),
-  appIcon: "/images/shared/logo.png",
-  transports: {
-    [bscTestnet.id]: http("https://bsc-testnet-rpc.publicnode.com"),
-    // [bsc.id]: http("https://bsc.blockrazor.xyz"),
-  },
-  ssr: true,
+	appName: "Shophekart",
+	appDescription: "A crypto ecommerce store",
+	projectId: "5414a59b644946d2fd01f5710c4784ad",
+	chains: [bsc],
+	appUrl: getOrigin(),
+	appIcon: "/images/shared/logo.png",
+	transports: {
+		[bsc.id]: http("https://bsc.blockrazor.xyz"),
+	},
+	ssr: true,
 });

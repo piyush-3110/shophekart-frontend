3b384b59004c4e411553e8ad2350aeb4dae08c3f
Umar <khanumar0108@gmail.com>
2024-11-06 10:29:32 +0530

    feat: Configure mainnet and testnet wagmi and contract configs

--- src/constants/contractConfig/index.ts
+++ src/constants/contractConfig/index.ts
@@ -9,36 +9,7 @@ import TOKEN_ADDRESS from "../tokenAddress";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type TContractConfig = Record<string, { address: `0x${string}`; abi: any }>;
 
-// TESTNET
-// const CONTRACT_CONFIG: TContractConfig = {
-// 	marketplace: {
-// 		address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
-// 		abi: MARKETPLACE_ABI,
-// 	},
-// 	escrow: {
-// 		address: "0x5540e5cf498B4D7278ad6D6C58E7800f2450204c",
-// 		abi: ESCROW_ABI,
-// 	},
-// 	usdt: {
-// 		address: TOKEN_ADDRESS.USDT,
-// 		abi: ERC20_ABI,
-// 	},
-// 	usdc: {
-// 		address: TOKEN_ADDRESS.USDC,
-// 		abi: ERC20_ABI,
-// 	},
-// 	cshop: {
-// 		address: TOKEN_ADDRESS.CSHOP,
-// 		abi: ERC20_ABI,
-// 	},
-// 	cshopTokenSale: {
-// 		address: "0xE11CC106ba82D28Ff07733d6cfb65AEbaf3F6C5d",
-// 		abi: CSHOP_TOKEN_SALE_ABI,
-// 	},
-// };
-
-// MAINNET
-const CONTRACT_CONFIG: TContractConfig = {
+const mainnetConfig: TContractConfig = {
 	marketplace: {
 		address: "0x9192702F30Eecf8209494Eeef5C38951e0142e73",
 		abi: MARKETPLACE_ABI,
@@ -64,5 +35,35 @@ const CONTRACT_CONFIG: TContractConfig = {
 		abi: CSHOP_TOKEN_SALE_ABI,
 	},
 };
+
+const testnetConfig: TContractConfig = {
+	marketplace: {
+		address: "0xc479963Bbc64f53a3bE9F0841611D440645cB20F",
+		abi: MARKETPLACE_ABI,
+	},
+	escrow: {
+		address: "0x5540e5cf498B4D7278ad6D6C58E7800f2450204c",
+		abi: ESCROW_ABI,
+	},
+	usdt: {
+		address: TOKEN_ADDRESS.USDT,
+		abi: ERC20_ABI,
+	},
+	usdc: {
+		address: TOKEN_ADDRESS.USDC,
+		abi: ERC20_ABI,
+	},
+	cshop: {
+		address: TOKEN_ADDRESS.CSHOP,
+		abi: ERC20_ABI,
+	},
+	cshopTokenSale: {
+		address: "0xE11CC106ba82D28Ff07733d6cfb65AEbaf3F6C5d",
+		abi: CSHOP_TOKEN_SALE_ABI,
+	},
+};
+
+const CONTRACT_CONFIG: TContractConfig =
+	process.env.NODE_ENV === "production" ? mainnetConfig : testnetConfig;
 
 export default CONTRACT_CONFIG;

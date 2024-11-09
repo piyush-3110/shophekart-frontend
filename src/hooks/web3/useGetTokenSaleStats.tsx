657b49008490dae910a138cc18f2843fc882b010
Piyush-Das <piyushdas3110@gmail.com>
2024-11-09 20:07:04 +0530

    update token to reduce number of rerenders

--- src/hooks/web3/useGetTokenSaleStats.tsx
+++ src/hooks/web3/useGetTokenSaleStats.tsx
@@ -1,45 +1,44 @@
 import CONTRACT_CONFIG from "@/constants/contractConfig";
 import { Abi, formatEther } from "viem";
 import { Config, useReadContract } from "wagmi";
+import { useEffect, useState } from "react";
 
 export default function useGetTokenSaleStats({ round = 1 }: IProps) {
-	const { data: soldTokens, isLoading: soldTokenLoading } = useReadContract<
-		Abi,
-		"getTokenPurchasedInRounds",
-		[number],
-		Config,
-		bigint
-	>({
+	const { data: soldTokens, isLoading: soldTokenLoading } = useReadContract<Abi, "getTokenPurchasedInRounds", [number], Config, bigint>({
 		...CONTRACT_CONFIG.cshopTokenSale,
 		functionName: "getTokenPurchasedInRounds",
 		args: [round],
 	});
 
-	const { data: maxTokens, isLoading: maxTokensLoading } = useReadContract<
-		Abi,
-		"maxTokenInround",
-		[],
-		Config,
-		bigint
-	>({
+	const { data: maxTokens, isLoading: maxTokensLoading } = useReadContract<Abi, "maxTokenInround", [], Config, bigint>({
 		...CONTRACT_CONFIG.cshopTokenSale,
 		functionName: "maxTokenInround",
 		args: [],
 	});
-console.log(soldTokens);
-console.log(maxTokens);
-	// Format soldTokens and maxTokens to ethers, then calculate percentage
-	const soldTokensFormatted = soldTokens ? Number(formatEther(soldTokens)) : 0;
-	const maxTokensFormatted = maxTokens ? Number(formatEther(maxTokens)) : 0;
-	const completionPercentage =
-		maxTokensFormatted > 0
-			? parseFloat(((soldTokensFormatted / maxTokensFormatted) * 100).toFixed(1))
-			: 0;
 
+	const [completionPercentage, setCompletionPercentage] = useState<number>(0);
+
+	useEffect(() => {
+		if (soldTokens && maxTokens) {
+			// Format soldTokens and maxTokens to ethers
+			const soldTokensFormatted = Number(formatEther(soldTokens));
+			const maxTokensFormatted = Number(formatEther(maxTokens));
+
+			// Calculate completion percentage
+			const percentage = maxTokensFormatted > 0
+				? parseFloat(((soldTokensFormatted / maxTokensFormatted) * 100).toFixed(1))
+				: 0;
+
+			// Set the calculated percentage
+			setCompletionPercentage(percentage);
+		}
+	}, [soldTokens, maxTokens]); // Only re-run when these values change
+
+	// Returning data from the hook
 	return {
-		soldTokens: soldTokensFormatted,
+		soldTokens: soldTokens ? Number(formatEther(soldTokens)) : 0,
 		soldTokenLoading,
-		maxTokens: maxTokensFormatted,
+		maxTokens: maxTokens ? Number(formatEther(maxTokens)) : 0,
 		maxTokensLoading,
 		completionPercentage, // Return calculated percentage
 	};

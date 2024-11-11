4579b1202fe65d508cbad463d4f3eef65a11fbf4
Piyush-Das <piyushdas3110@gmail.com>
2024-11-11 17:17:06 +0530

    added

--- src/components/Homepage/Roadmap/Roadmap.tsx
+++ src/components/Homepage/Roadmap/Roadmap.tsx
@@ -1,11 +1,22 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import Image from "next/image";
 
 export const Roadmap = () => {
   return (
-    <div className='bg-black lg:min-h-[80vh] flex flex-col items-center lg:justify-center py-10 gap-12'>
-       <h1 className='text-lg md:text-3xl text-white lg:mb-14 font-semibold'>RoadMap</h1>
-        <Image src="/images/homepage/roadmap.png" height={1024} width={1024} className='w-full h-auto' alt='Roadmap'></Image>
+    <div className="bg-black lg:min-h-[80vh] flex flex-col items-center lg:justify-center py-10 gap-6 relative">
+      <h1 className="text-lg md:text-3xl text-white font-semibold mb-4">
+        RoadMap
+      </h1>
+
+      {/* Roadmap Image */}
+      <Image
+        src="/images/homepage/roadmap.png"
+        height={1024}
+        width={1024}
+        className="w-full h-auto mt-24" // Margin to avoid overlap
+        alt="Roadmap"
+      />
     </div>
-  )
-}
+  );
+};

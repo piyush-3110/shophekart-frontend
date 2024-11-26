9dfd9608eb985388ff71bfe1c40e68c84e947d20
Piyush-Das <piyushdas3110@gmail.com>
2024-11-26 14:08:11 +0530

    integrated chatgpt model

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,4 +1,5 @@
 export const envConfig = {
   BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
   GATEWAY_URL: process.env.NEXT_PUBLIC_PINATA_GATEWAY as string,
+  CHAT_API: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 };
